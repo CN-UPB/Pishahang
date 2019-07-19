d2dc2744b3b3d7349ff9fa60cf8f25090db9cf38
sanketgupta99 <skg.sanket@gmail.com>
2019-07-19 16:48:16 +0200

    Updated main_auto code.

--- son-mano-framework/plugins/son-mano-mv/son_mano_mv/multi_version/main_auto.py
+++ son-mano-framework/plugins/son-mano-mv/son_mano_mv/multi_version/main_auto.py
@@ -41,102 +41,102 @@ def main_auto(_method, _scenario):
         sleep(1)
         random.seed(seed)
 
-    # read scenario input
-    # if len(sys.argv) < 3:
-    #     print("MIP usage: python3 main.py mip <scenario>")
-    #     print("Heuristic usage: python3 main.py heuristic <scenario>")
-    #     print("Pareto usage: python3 main.py pareto <scenario> <objective> <bound1> <bound2> <bound3>")
-    #     exit(1)
-    method = _method
-    scenario = _scenario
-    # nodes, links, templates, sources, fixed, prev_embedding, events = reader.read_scenario(scenario)
-    nodes, links, templates, sources, fixed, prev_embedding, events = reader.read_scenario(scenario)
+        # read scenario input
+        # if len(sys.argv) < 3:
+        #     print("MIP usage: python3 main.py mip <scenario>")
+        #     print("Heuristic usage: python3 main.py heuristic <scenario>")
+        #     print("Pareto usage: python3 main.py pareto <scenario> <objective> <bound1> <bound2> <bound3>")
+        #     exit(1)
+        method = _method
+        scenario = _scenario
+        # nodes, links, templates, sources, fixed, prev_embedding, events = reader.read_scenario(scenario)
+        nodes, links, templates, sources, fixed, prev_embedding, events = reader.read_scenario(scenario)
 
-    for s in sources:
-        s.dr = s.dr * data_rate_parameter
+        for s in sources:
+            s.dr = s.dr * data_rate_parameter
 
-    # solve with MIP
-    if method == "mip":
-        # model = tep_mulv.solve(nodes, links, templates, prev_embedding, sources, fixed) 
-        # model = tep_mulv_proctime.solve(nodes, links, templates, prev_embedding, sources, fixed)    
-        model = tep_mulv_lex.solve(nodes, links, templates, prev_embedding, sources, fixed) 
-        # model = tep_extended.solve(nodes, links, templates, prev_embedding, sources, fixed, scenario, obj)
-        writer.write_mip_result(model, scenario, nodes, links, obj, sources, fixed, seed)
+        # solve with MIP
+        if method == "mip":
+            # model = tep_mulv.solve(nodes, links, templates, prev_embedding, sources, fixed)
+            # model = tep_mulv_proctime.solve(nodes, links, templates, prev_embedding, sources, fixed)
+            model = tep_mulv_lex.solve(nodes, links, templates, prev_embedding, sources, fixed)
+            # model = tep_extended.solve(nodes, links, templates, prev_embedding, sources, fixed, scenario, obj)
+            writer.write_mip_result(model, scenario, nodes, links, obj, sources, fixed, seed)
 
-    elif method == "drawnet":
-        NG = nx.DiGraph()
-        for nid in nodes.ids:
-            NG.add_node(nid)
-        for eid in links.ids:
-            NG.add_edge(*eid)
+        elif method == "drawnet":
+            NG = nx.DiGraph()
+            for nid in nodes.ids:
+                NG.add_node(nid)
+            for eid in links.ids:
+                NG.add_edge(*eid)
 
-        label_delay = {}
-        # print(links.delay)
-        for l in links.delay.keys():
-            # print(l,links.delay[l])
-            label_delay[l] = round(links.delay[l])
-        # label_delay = links.delay
-        # pos = nx.shell_layout(NG)
-        pos = nx.spring_layout(NG, iterations=100)
-        nx.draw_networkx_nodes(NG, pos, node_color="red")
-        nx.draw_networkx_labels(NG, pos, font_size=12)
-        # nx.draw_networkx_edge_labels(NG, pos, edge_labels=label_delay, label_pos = 0.4, font_size=4)
-        nx.draw_networkx_edges(NG, pos)
-        plt.axis("off")
-        plt.savefig(str(scenario)+"_netgraph.pdf")  
-        # plt.show()
-
-
-    # solve with MIP; optimizing one objective and bounding the others
-    elif method == "pareto":
-        # get objective and bounds from arguments
-        obj = objective.get_objective(sys.argv[3])
-        # bounds have to be ordered: over-sub, changed, resources, delay (without the one that's optimized)
-        bounds = (float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6]))
-        # run with specified objective and bounds
-        model = tep_extended.solve(nodes, links, templates, prev_embedding, sources, fixed, scenario, obj, bounds)
-        writer.write_mip_result(model, scenario, nodes, links, obj, sources, bounds=bounds)
+            label_delay = {}
+            # print(links.delay)
+            for l in links.delay.keys():
+                # print(l,links.delay[l])
+                label_delay[l] = round(links.delay[l])
+            # label_delay = links.delay
+            # pos = nx.shell_layout(NG)
+            pos = nx.spring_layout(NG, iterations=100)
+            nx.draw_networkx_nodes(NG, pos, node_color="red")
+            nx.draw_networkx_labels(NG, pos, font_size=12)
+            # nx.draw_networkx_edge_labels(NG, pos, edge_labels=label_delay, label_pos = 0.4, font_size=4)
+            nx.draw_networkx_edges(NG, pos)
+            plt.axis("off")
+            plt.savefig(str(scenario)+"_netgraph.pdf")
+            # plt.show()
 
 
-    # solve with heuristic
-    elif method == "heuristic":
-        # use specified or random seed
-        # if len(sys.argv) >= 4:
-        #     seed = int(sys.argv[3])
-        #     seed_subfolder = True       # put result in sub-folder of the chosen seed
-        # else:
-        seed = random.randint(0, 9999)
-        seed_subfolder = False
-        random.seed(seed)
-        print("Using seed {}".format(seed))
-
-        # set up logging into file Data/logs/heuristic/scenario_timestamp_seed.log
-        # logging.disable(logging.CRITICAL)     # disable logging
-        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        os.makedirs("results/logs/heuristic/obj{}".format(obj), exist_ok=True)
-        logging.basicConfig(filename="results/logs/heuristic/obj{}/{}_{}_{}.log".format(obj, os.path.basename(scenario)[:-4], timestamp, seed),
-                            level=logging.DEBUG, format="%(asctime)s(%(levelname)s):\t%(message)s", datefmt="%H:%M:%S")
-
-        logging.info("Starting initial embedding at {}".format(timestamp))
+        # solve with MIP; optimizing one objective and bounding the others
+        elif method == "pareto":
+            # get objective and bounds from arguments
+            obj = objective.get_objective(sys.argv[3])
+            # bounds have to be ordered: over-sub, changed, resources, delay (without the one that's optimized)
+            bounds = (float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6]))
+            # run with specified objective and bounds
+            model = tep_extended.solve(nodes, links, templates, prev_embedding, sources, fixed, scenario, obj, bounds)
+            writer.write_mip_result(model, scenario, nodes, links, obj, sources, bounds=bounds)
 
 
-        print("Initial embedding\n")
-        init_time, runtime, obj_value, changed, overlays = control.solve(nodes, links, templates, {}, sources, fixed, obj)
-        writer.write_heuristic_result(init_time, runtime, obj_value, changed, overlays.values(), scenario, obj, -1, "Initial embedding", nodes, links, seed, seed_subfolder, sources)
+        # solve with heuristic
+        elif method == "heuristic":
+            # use specified or random seed
+            # if len(sys.argv) >= 4:
+            #     seed = int(sys.argv[3])
+            #     seed_subfolder = True       # put result in sub-folder of the chosen seed
+            # else:
+            seed = random.randint(0, 9999)
+            seed_subfolder = False
+            random.seed(seed)
+            print("Using seed {}".format(seed))
 
-        # if events exists, update input accordingly and solve again for each event until last event is reached
-        event_no = 0
-        while events is not None and event_no is not None:
-            print("\n------------------------------------------------\n")
-            logging.info("\n------------------------------------------------\n")
-            logging.info("Embedding event {} at {}".format(event_no, datetime.now().strftime("%Y-%m-%d_%H-%M-%S")))
+            # set up logging into file Data/logs/heuristic/scenario_timestamp_seed.log
+            # logging.disable(logging.CRITICAL)     # disable logging
+            timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            os.makedirs("results/logs/heuristic/obj{}".format(obj), exist_ok=True)
+            logging.basicConfig(filename="results/logs/heuristic/obj{}/{}_{}_{}.log".format(obj, os.path.basename(scenario)[:-4], timestamp, seed),
+                                level=logging.DEBUG, format="%(asctime)s(%(levelname)s):\t%(message)s", datefmt="%H:%M:%S")
 
-            new_no, event, templates, sources, fixed = reader.read_event(events, event_no, templates, sources, fixed)
-            init_time, runtime, obj_value, changed, overlays = control.solve(nodes, links, templates, overlays, sources, fixed, obj)
-            writer.write_heuristic_result(init_time, runtime, obj_value, changed, overlays.values(), scenario, obj, event_no, event, nodes, links, seed, seed_subfolder, sources)
-            event_no = new_no
+            logging.info("Starting initial embedding at {}".format(timestamp))
 
 
-    # invalid method
-    else:
-        print("Invalid solving method: {}. Use 'mip' or 'heuristic'".format(method))
+            print("Initial embedding\n")
+            init_time, runtime, obj_value, changed, overlays = control.solve(nodes, links, templates, {}, sources, fixed, obj)
+            writer.write_heuristic_result(init_time, runtime, obj_value, changed, overlays.values(), scenario, obj, -1, "Initial embedding", nodes, links, seed, seed_subfolder, sources)
+
+            # if events exists, update input accordingly and solve again for each event until last event is reached
+            event_no = 0
+            while events is not None and event_no is not None:
+                print("\n------------------------------------------------\n")
+                logging.info("\n------------------------------------------------\n")
+                logging.info("Embedding event {} at {}".format(event_no, datetime.now().strftime("%Y-%m-%d_%H-%M-%S")))
+
+                new_no, event, templates, sources, fixed = reader.read_event(events, event_no, templates, sources, fixed)
+                init_time, runtime, obj_value, changed, overlays = control.solve(nodes, links, templates, overlays, sources, fixed, obj)
+                writer.write_heuristic_result(init_time, runtime, obj_value, changed, overlays.values(), scenario, obj, event_no, event, nodes, links, seed, seed_subfolder, sources)
+                event_no = new_no
+
+
+        # invalid method
+        else:
+            print("Invalid solving method: {}. Use 'mip' or 'heuristic'".format(method))
