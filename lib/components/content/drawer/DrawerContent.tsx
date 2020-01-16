9277b3982f3cbbb2e6609137ee0256db0f2d7ebe
bjoluc <25661029+bjoluc@users.noreply.github.com>
2020-01-16 15:42:15 +0100

    integrate drawer into Page component

--- lib/components/content/drawer/DrawerContent.tsx
+++ lib/components/content/drawer/DrawerContent.tsx
@@ -1,3 +1,31 @@
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import MailIcon from "@material-ui/icons/Mail";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
 import * as React from "react";
 
-export const DrawerContent: React.FunctionComponent = () => <></>;
+export const DrawerContent: React.FunctionComponent = () => (
+  <>
+    <Divider />
+    <List>
+      {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+        <ListItem button key={text}>
+          <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+          <ListItemText primary={text} />
+        </ListItem>
+      ))}
+    </List>
+    <Divider />
+    <List>
+      {["All mail", "Trash", "Spam"].map((text, index) => (
+        <ListItem button key={text}>
+          <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+          <ListItemText primary={text} />
+        </ListItem>
+      ))}
+    </List>
+  </>
+);
--- lib/components/layout/Page.tsx
+++ lib/components/layout/Page.tsx
@@ -1,7 +1,56 @@
-import { MainGrid } from "./MainGrid";
-import { ToolBar } from "./toolbar/ToolBar";
+import {
+  AppBar,
+  Hidden,
+  IconButton,
+  Drawer as MaterialDrawer,
+  Theme,
+  Toolbar,
+  Typography,
+  createStyles,
+  makeStyles,
+  useTheme,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 import Head from "next/head";
 import * as React from "react";
+
+import { DrawerContent } from "../content/drawer/DrawerContent";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    drawer: {
+      [theme.breakpoints.up("sm")]: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+    },
+    appBar: {
+      [theme.breakpoints.up("sm")]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+      },
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up("sm")]: {
+        display: "none",
+      },
+    },
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  })
+);
 
 type Props = {
   /**
@@ -13,23 +62,86 @@ type Props = {
    */
   titleAddHomepageTitle?: boolean;
   /**
-   * The `MainGrid`'s `Container`'s `maxWidth` property. Defaults to "lg".
+   * Whether or not to hide the drawer. Defaults to false.
    */
-  maxWidth?: false | "xs" | "sm" | "md" | "lg" | "xl";
+  hideDrawer?: boolean;
 };
 
 export const Page: React.FunctionComponent<Props> = ({
   title = "",
   titleAddHomepageTitle = true,
-  maxWidth = "lg",
+  hideDrawer = false,
   children,
 }) => {
+  const classes = useStyles({});
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <DrawerContent />
+    </div>
+  );
+
   return (
-    <div id="page">
+    <div className={classes.root}>
       <Head>
         <title>{title + (titleAddHomepageTitle ? " – Pishahang" : "")}</title>
       </Head>
-      <MainGrid maxWidth={maxWidth}>{children}</MainGrid>
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Responsive drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        <Hidden smUp implementation="css">
+          <MaterialDrawer
+            variant="temporary"
+            anchor={"left"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </MaterialDrawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <MaterialDrawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </MaterialDrawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        Content
+      </main>
     </div>
   );
 };
