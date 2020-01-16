1c1801841cb8895b52eac476e6a4c2d68661df6f
Abdul Basit Tahir <abtcr7@gmail.com>
2020-01-16 19:30:47 +0100

    LinkedList icons, names and overall UI updated.

--- lib/components/content/drawer/DrawerContent.tsx
+++ lib/components/content/drawer/DrawerContent.tsx
@@ -3,29 +3,30 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
+import { Computer, Dns, Dock, Dvr, Person, PowerSettingsNew, WebAsset } from "@material-ui/icons";
 import MailIcon from "@material-ui/icons/Mail";
 import InboxIcon from "@material-ui/icons/MoveToInbox";
 import * as React from "react";
+
+import { LinkedListItem } from "../../layout/LinkedListItem";
 
 export const DrawerContent: React.FunctionComponent = () => (
   <>
     <Divider />
     <List>
-      {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-        <ListItem button key={text}>
-          <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-          <ListItemText primary={text} />
-        </ListItem>
-      ))}
+      <LinkedListItem text={"User Profile"} icon={Person} href={""}></LinkedListItem>
+      <LinkedListItem text={"Monitor"} icon={Dvr} href={""}></LinkedListItem>
     </List>
     <Divider />
     <List>
-      {["All mail", "Trash", "Spam"].map((text, index) => (
-        <ListItem button key={text}>
-          <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-          <ListItemText primary={text} />
-        </ListItem>
-      ))}
+      <LinkedListItem text={"Services"} icon={Dns} href={""}></LinkedListItem>
+      <LinkedListItem text={"Virtual Machines"} icon={Computer} href={""}></LinkedListItem>
+      <LinkedListItem text={"Containers"} icon={WebAsset} href={""}></LinkedListItem>
     </List>
+    <Divider />
+    <List>
+      <LinkedListItem text={"Logout"} icon={PowerSettingsNew} href={""}></LinkedListItem>
+    </List>
+    <Divider />
   </>
 );
