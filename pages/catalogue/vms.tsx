2a4df12fc6eca161c2a6414e21754801d07d86fe
Abdul Basit Tahir <abtcr7@gmail.com>
2020-01-20 17:33:38 +0100

    Table reference added in the VNF page

--- pages/catalogue/vms.tsx
+++ pages/catalogue/vms.tsx
@@ -1,9 +1,14 @@
 import { NextPage } from "next";
 
 import { Page } from "../../lib/components/layout/Page";
+import { VnfdTable } from "../../lib/components/layout/tables/VnfdTable";
 
 const VirtualMachinesPage: NextPage = () => {
-  return <Page title="Virtual Machines"></Page>;
+  return (
+    <Page title="Virtual Machines">
+      <VnfdTable></VnfdTable>
+    </Page>
+  );
 };
 
 export default VirtualMachinesPage;
