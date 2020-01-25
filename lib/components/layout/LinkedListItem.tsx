f06235cf512d3736d313679220e6acc41566357f
bjoluc <mail@bjoluc.de>
2020-01-25 23:35:39 +0100

    Highlight selected drawer entry

--- lib/components/layout/LinkedListItem.tsx
+++ lib/components/layout/LinkedListItem.tsx
@@ -1,5 +1,6 @@
 import { ListItemIcon, ListItemText } from "@material-ui/core";
 import { SvgIconComponent } from "@material-ui/icons";
+import { useRouter } from "next/router";
 import * as React from "react";
 
 import { ListItemLink } from "../links/ListItemLink";
@@ -10,9 +11,19 @@ type LinkedListItemProps = {
   icon: SvgIconComponent;
 };
 
-export const LinkedListItem: React.FunctionComponent<LinkedListItemProps> = props => (
-  <ListItemLink button color="inherit" key={props.text} href={props.href}>
-    <ListItemIcon>{<props.icon />}</ListItemIcon>
-    <ListItemText primary={props.text} />
-  </ListItemLink>
-);
+export const LinkedListItem: React.FunctionComponent<LinkedListItemProps> = props => {
+  const router = useRouter();
+
+  return (
+    <ListItemLink
+      button
+      color="inherit"
+      key={props.text}
+      href={props.href}
+      selected={router.pathname === props.href}
+    >
+      <ListItemIcon>{<props.icon />}</ListItemIcon>
+      <ListItemText primary={props.text} />
+    </ListItemLink>
+  );
+};
