cdb392cde84f1531af4b0fe68e353ed236e59c50
Abdul Basit Tahir <abtcr7@gmail.com>
2020-01-27 16:21:56 +0100

    added floating action button for uploading descriptors

--- pages/descriptors/cns.tsx
+++ pages/descriptors/cns.tsx
@@ -1,3 +1,5 @@
+import Fab from "@material-ui/core/Fab";
+import { CloudUpload } from "@material-ui/icons";
 import { NextPage } from "next";
 
 import { Page } from "../../lib/components/layout/Page";
@@ -6,6 +8,9 @@ import { VnfdTable } from "../../lib/components/layout/tables/VnfdTable";
 const ContainersPage: NextPage = () => {
   return (
     <Page title="CN Based VNF Descriptors">
+      <Fab variant="extended" color="primary" size="small" style={{ float: "right" }}>
+        <CloudUpload />
+      </Fab>
       <VnfdTable pageName="Container"></VnfdTable>
     </Page>
   );
--- pages/descriptors/vms.tsx
+++ pages/descriptors/vms.tsx
@@ -1,3 +1,5 @@
+import Fab from "@material-ui/core/Fab";
+import { CloudUpload } from "@material-ui/icons";
 import { NextPage } from "next";
 
 import { Page } from "../../lib/components/layout/Page";
@@ -6,6 +8,9 @@ import { VnfdTable } from "../../lib/components/layout/tables/VnfdTable";
 const VirtualMachinesPage: NextPage = () => {
   return (
     <Page title="VM Based VNF Descriptors">
+      <Fab variant="extended" color="primary" size="small" style={{ float: "right" }}>
+        <CloudUpload />
+      </Fab>
       <VnfdTable></VnfdTable>
     </Page>
   );
