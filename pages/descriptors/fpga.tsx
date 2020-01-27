b22d632b88304d83bda98615b3670a59d9ed1f56
Abdul Basit Tahir <abtcr7@gmail.com>
2020-01-27 16:21:14 +0100

    added coming soon text

--- pages/descriptors/fpga.tsx
+++ pages/descriptors/fpga.tsx
@@ -3,7 +3,7 @@ import { NextPage } from "next";
 import { Page } from "../../lib/components/layout/Page";
 
 const ContainersPage: NextPage = () => {
-  return <Page title="FPGA Based VNF Descriptors"></Page>;
+  return <Page title="FPGA Based VNF Descriptors">Coming Soon...</Page>;
 };
 
 export default ContainersPage;
