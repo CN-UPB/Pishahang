6a5b26430b913bd8432504266a498f675d1cd8ea
Abdul Basit Tahir <abtcr7@gmail.com>
2020-01-30 17:56:35 +0100

    Graphs added to Dashboard and Monitor Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import { NextPage } from "next";
 
+import DashBoardGraph from "../lib/components/layout/charts/DashboardGraph";
 import { Page } from "../lib/components/layout/Page";
 
 const DashboardPage: NextPage = () => {
-  return <Page title="Dashboard">Welcome to Pishahang!</Page>;
+  return (
+    <Page title="Dashboard">
+      <DashBoardGraph></DashBoardGraph>
+    </Page>
+  );
 };
 
 export default DashboardPage;
--- pages/monitor.tsx
+++ pages/monitor.tsx
@@ -1,9 +1,14 @@
 import { NextPage } from "next";
 
+import MonitorGraph from "../lib/components/layout/charts/Monitoring";
 import { Page } from "../lib/components/layout/Page";
 
 const MonitoringPage: NextPage = () => {
-  return <Page title="Monitoring"></Page>;
+  return (
+    <Page title="Monitoring">
+      <MonitorGraph></MonitorGraph>
+    </Page>
+  );
 };
 
 export default MonitoringPage;
