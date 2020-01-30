cdbad7c86a7265832d7825a0395d78100c813f59
Abdul Basit Tahir <abtcr7@gmail.com>
2020-01-31 00:02:47 +0100

    added dummy data to be displayed based on descriptor information

--- pages/descriptors/cns.tsx
+++ pages/descriptors/cns.tsx
@@ -1,17 +1,39 @@
 import Fab from "@material-ui/core/Fab";
-import { CloudUpload } from "@material-ui/icons";
+import { CloudUpload, DateRange } from "@material-ui/icons";
 import { NextPage } from "next";
 
 import { Page } from "../../lib/components/layout/Page";
 import { VnfdTable } from "../../lib/components/layout/tables/VnfdTable";
+import { VnfdMeta } from "../../lib/models/VnfdMeta";
 
 const ContainersPage: NextPage = () => {
+  const data: VnfdMeta[] = [
+    {
+      status: "active",
+      signature: "null",
+      md5: "43c1b014db0ce887288ea95391b0cd10",
+      updated_at: new Date(),
+      created_at: new Date(),
+      username: "null",
+      descriptor: {
+        name: "forwarder-vm-vnf",
+        vendor: "eu.sonata-nfv.cloud-service-descriptor",
+        version: "1.0",
+        author: "Elton John",
+        description: "ICMP ping request forwarder; container-based VNF.",
+        descriptor_version: "2.0",
+        virtual_deployment_units: "",
+      },
+      uuid: "66695e0f-5472-4f85-a310-fe0676bf28e6",
+    },
+  ];
+
   return (
     <Page title="CN Based VNF Descriptors">
       <Fab color="secondary" size="small" style={{ float: "right" }} aria-label="Upload">
         <CloudUpload />
       </Fab>
-      <VnfdTable pageName="Container"></VnfdTable>
+      <VnfdTable pageName="Container" data={data}></VnfdTable>
     </Page>
   );
 };
--- pages/descriptors/vms.tsx
+++ pages/descriptors/vms.tsx
@@ -4,14 +4,36 @@ import { NextPage } from "next";
 
 import { Page } from "../../lib/components/layout/Page";
 import { VnfdTable } from "../../lib/components/layout/tables/VnfdTable";
+import { VnfdMeta } from "../../lib/models/VnfdMeta";
 
 const VirtualMachinesPage: NextPage = () => {
+  const data: VnfdMeta[] = [
+    {
+      status: "active",
+      signature: "null",
+      md5: "a051247412cc8f0d68ad62f6c0b68fbb",
+      updated_at: new Date(),
+      created_at: new Date(),
+      username: "null",
+      descriptor: {
+        name: "forwarder-vm-vnf",
+        vendor: "eu.sonata-nfv.vnf-descriptor",
+        version: "1.0",
+        author: "Elton John",
+        description: "ICMP ping request forwarder; VM-based VNF",
+        descriptor_version: "vnfd-schema-01",
+        virtual_deployment_units: "",
+      },
+      uuid: "d885fbd7-e474-41a1-8c11-628024984210",
+    },
+  ];
+
   return (
     <Page title="VM Based VNF Descriptors">
       <Fab color="primary" size="small" style={{ float: "right" }} aria-label="Upload">
         <CloudUpload />
       </Fab>
-      <VnfdTable></VnfdTable>
+      <VnfdTable data={data}></VnfdTable>
     </Page>
   );
 };
