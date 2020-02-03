dcda3392493d72a910307c25f9c4ffcb8db071ea
Abdul Basit Tahir <abtcr7@gmail.com>
2020-02-03 18:18:36 +0100

    dialog updated with tabular structure, date converted

--- lib/components/layout/charts/Monitoring.tsx
+++ lib/components/layout/charts/Monitoring.tsx
@@ -1,5 +1,7 @@
 import React, { PureComponent } from "react";
 import {
+  Area,
+  AreaChart,
   CartesianGrid,
   Legend,
   Line,
@@ -12,45 +14,45 @@ import {
 
 const data = [
   {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
+    name: "Time A",
+    CPU: 4000,
+    RAM: 2400,
     amt: 2400,
   },
   {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
+    name: "Time B",
+    CPU: 3000,
+    RAM: 1398,
     amt: 2210,
   },
   {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
+    name: "Time C",
+    CPU: 2000,
+    RAM: 9800,
     amt: 2290,
   },
   {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
+    name: "Time D",
+    CPU: 2780,
+    RAM: 3908,
     amt: 2000,
   },
   {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
+    name: "Time E",
+    CPU: 1890,
+    RAM: 4800,
     amt: 2181,
   },
   {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
+    name: "Time F",
+    CPU: 2390,
+    RAM: 3800,
     amt: 2500,
   },
   {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
+    name: "Time G",
+    CPU: 3490,
+    RAM: 4300,
     amt: 2100,
   },
 ];
@@ -61,23 +63,39 @@ export default class MonitorGraph extends PureComponent {
   render() {
     return (
       <ResponsiveContainer width="100%" height={300}>
-        <LineChart
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
+        <AreaChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            </linearGradient>
+          </defs>
           <XAxis dataKey="name" />
           <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
           <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-        </LineChart>
+          <Area
+            type="monotone"
+            dataKey="CPU"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#colorUv)"
+          />
+          <Area
+            type="monotone"
+            dataKey="RAM"
+            stroke="#82ca9d"
+            fillOpacity={1}
+            fill="url(#colorPv)"
+          />
+          <Legend verticalAlign="top" height={36} />
+          <Line name="pv of pages" type="monotone" dataKey="CPU" stroke="#8884d8" />
+          <Line name="uv of pages" type="monotone" dataKey="RAM" stroke="#82ca9d" />
+        </AreaChart>
       </ResponsiveContainer>
     );
   }
--- lib/hooks/useVnfdInfoDialog.tsx
+++ lib/hooks/useVnfdInfoDialog.tsx
@@ -1,4 +1,13 @@
-import { Button } from "@material-ui/core";
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+} from "@material-ui/core";
 import * as React from "react";
 import { useModal } from "react-modal-hook";
 
@@ -22,7 +31,65 @@ export function useVnfdInfoDialog(): (vnfdMeta: VnfdMeta) => void {
         </>
       }
     >
-      {data.descriptor.description}
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableBody>
+            <TableRow key={data.descriptor.description}>
+              <TableCell component="th" scope="row">
+                <Typography variant="body2" gutterBottom>
+                  Description:
+                </Typography>
+              </TableCell>
+              <TableCell align="left">{data.descriptor.description}</TableCell>
+            </TableRow>
+
+            <TableRow key={data.descriptor.descriptor_version}>
+              <TableCell component="th" scope="row">
+                <Typography variant="body2" gutterBottom>
+                  Descriptor Version:
+                </Typography>
+              </TableCell>
+              <TableCell align="left">{data.descriptor.descriptor_version}</TableCell>
+            </TableRow>
+
+            <TableRow key={data.descriptor.version}>
+              <TableCell component="th" scope="row">
+                <Typography variant="body2" gutterBottom>
+                  Version:
+                </Typography>
+              </TableCell>
+              <TableCell align="left">{data.descriptor.version}</TableCell>
+            </TableRow>
+
+            <TableRow key={data.descriptor.vendor}>
+              <TableCell component="th" scope="row">
+                <Typography variant="body2" gutterBottom>
+                  Vendor:
+                </Typography>
+              </TableCell>
+              <TableCell align="left">{data.descriptor.vendor}</TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell component="th" scope="row">
+                <Typography variant="body2" gutterBottom>
+                  Created At:
+                </Typography>
+              </TableCell>
+              <TableCell align="left">{data.created_at.toString()}</TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell component="th" scope="row">
+                <Typography variant="body2" gutterBottom>
+                  Updated At:
+                </Typography>
+              </TableCell>
+              <TableCell align="left">{data.updated_at.toString()}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </GenericDialog>
   ));
 
