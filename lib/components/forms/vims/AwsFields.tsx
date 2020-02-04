6942a07ec8727294fc6be4cb736dfcc8d46f1c8f
bjoluc <mail@bjoluc.de>
2020-02-04 15:46:08 +0100

    Remove grid containers from form field components

--- lib/components/forms/vims/AwsFields.tsx
+++ lib/components/forms/vims/AwsFields.tsx
@@ -1,34 +1,14 @@
-import {
-  Box,
-  Button,
-  Container,
-  FormControl,
-  FormHelperText,
-  Grid,
-  InputLabel,
-  MenuItem,
-  Select,
-  Typography,
-  makeStyles,
-} from "@material-ui/core";
-import { Form, Formik } from "formik";
+import { Grid } from "@material-ui/core";
 import { TextField } from "formik-material-ui";
 import * as React from "react";
-import * as Yup from "yup";
 
-export const AwsFields: React.FunctionComponent = () => {
-  const initialFormValues = {};
-
-  return (
-    <>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <TextField name="accessKey" label="Access Key" />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField name="secretKey" label="Secret Key" />
-        </Grid>
-      </Grid>
-    </>
-  );
-};
+export const AwsFields: React.FunctionComponent = () => (
+  <>
+    <Grid item xs={6}>
+      <TextField name="accessKey" label="Access Key" />
+    </Grid>
+    <Grid item xs={6}>
+      <TextField name="secretKey" label="Secret Key" />
+    </Grid>
+  </>
+);
--- lib/components/forms/vims/KubernetesFields.tsx
+++ lib/components/forms/vims/KubernetesFields.tsx
@@ -1,36 +1,17 @@
-import {
-  Box,
-  Button,
-  Container,
-  FormControl,
-  FormHelperText,
-  Grid,
-  InputLabel,
-  MenuItem,
-  Select,
-  Typography,
-  makeStyles,
-} from "@material-ui/core";
-import { Form, Formik } from "formik";
+import { Grid } from "@material-ui/core";
 import { TextField } from "formik-material-ui";
 import * as React from "react";
 
-export const KubernetesFields: React.FunctionComponent = () => {
-  const initialFormValues = {};
-
-  return (
-    <>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <TextField name="vimAddress" label="VIM Address" />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField name="serviceToken" label="Service Token" />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField name="cCC" label="Cluster CA Certificate" />
-        </Grid>
-      </Grid>
-    </>
-  );
-};
+export const KubernetesFields: React.FunctionComponent = () => (
+  <>
+    <Grid item xs={6}>
+      <TextField name="vimAddress" label="VIM Address" />
+    </Grid>
+    <Grid item xs={6}>
+      <TextField name="serviceToken" label="Service Token" />
+    </Grid>
+    <Grid item xs={6}>
+      <TextField name="cCC" label="Cluster CA Certificate" />
+    </Grid>
+  </>
+);
