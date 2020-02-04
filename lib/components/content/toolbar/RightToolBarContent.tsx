051c0364ebe3178c897a00671d63b16d0f7f6f66
bjoluc <mail@bjoluc.de>
2020-02-04 21:14:13 +0100

    Implement toolbar buttons

--- lib/components/content/toolbar/RightToolBarContent.tsx
+++ lib/components/content/toolbar/RightToolBarContent.tsx
@@ -1,16 +1,24 @@
 import { IconButton } from "@material-ui/core";
 import { PowerSettingsNew as LogoutIcon, Person as PersonIcon } from "@material-ui/icons";
 import * as React from "react";
+import { useDispatch } from "react-redux";
+
+import { logout } from "../../../store/actions/auth";
+import { IconButtonLink } from "../../links/IconButtonLink";
 
 export const RightToolBarContent: React.FunctionComponent<React.HTMLAttributes<
   HTMLDivElement
->> = props => (
-  <div {...props}>
-    <IconButton color={"inherit"}>
-      <PersonIcon />
-    </IconButton>
-    <IconButton color={"inherit"}>
-      <LogoutIcon />
-    </IconButton>
-  </div>
-);
+>> = props => {
+  const dispatch = useDispatch();
+
+  return (
+    <div {...props}>
+      <IconButtonLink color={"inherit"} href="/user">
+        <PersonIcon />
+      </IconButtonLink>
+      <IconButton color={"inherit"} onClick={() => dispatch(logout())}>
+        <LogoutIcon />
+      </IconButton>
+    </div>
+  );
+};
