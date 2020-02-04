6ec52ca2935bd6b312f894beb1f955b984d950ca
Abdul Basit Tahir <abtcr7@gmail.com>
2020-02-04 17:32:11 +0100

    descriptor editor dialog

---
+++ lib/components/content/DescriptorEditor.tsx
@@ -0,0 +1,24 @@
+import AceEditor from "react-ace";
+
+const textEditor = props => (
+  <div>
+    <AceEditor
+      mode={props.lan}
+      theme={props.theme}
+      onChange={props.onChange}
+      onLoad={props.onLoad}
+      defaultValue={props.defaultValue}
+      value={props.value}
+      name="UNIQUE_ID_OF_DIV"
+      editorProps={{
+        $blockScrolling: true,
+      }}
+      highlightActiveLine={true}
+      fontSize={21}
+      height="500px"
+      width="500px"
+    />
+  </div>
+);
+
+export default textEditor;
