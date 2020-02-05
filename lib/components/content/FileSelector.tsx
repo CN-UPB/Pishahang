d3ec225f69615400bcd98fc2b48cd0d60a5e4484
Abdul Basit Tahir <abtcr7@gmail.com>
2020-02-05 20:32:36 +0100

    Descriptor Uploader Hook

---
+++ lib/components/content/FileSelector.tsx
@@ -0,0 +1,34 @@
+import { DropzoneArea } from "material-ui-dropzone";
+import * as React from "react";
+
+type Props = {
+  /** Maximum number of files that can be loaded into the dropzone*/
+  filesLimit: number;
+  /** Shows previews BELOW the Dropzone*/
+  showPreviews: boolean;
+  /** Text in dropzone*/
+  dropzoneText: string;
+  /** Maximum file size (in MB) that the dropzone will accept*/
+  maxFileSize: number;
+  /** Shows styled snackbar alerts when files are dropped, deleted or rejected.*/
+  showAlerts: boolean;
+  /** A list of file mime types to accept. Does support wildcards. e.g. 'image/*', 'video/*', 'application/*'*/
+  acceptedFiles: string[];
+};
+
+export const FileSelector: React.FunctionComponent<Props> = props => {
+  let fileSizeInBytes = props.maxFileSize * 1048576;
+  function handleChange(files) {
+    console.log(files);
+  }
+  return (
+    <DropzoneArea
+      dropzoneText={props.dropzoneText}
+      acceptedFiles={props.acceptedFiles}
+      filesLimit={props.filesLimit}
+      maxFileSize={fileSizeInBytes}
+      showPreviews={props.showPreviews}
+      onChange={handleChange}
+    />
+  );
+};
