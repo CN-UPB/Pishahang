0edccda26838558bab131e7aa5440171a8b32cd4
Abdul Basit Tahir <abtcr7@gmail.com>
2020-02-06 18:28:44 +0100

    Added service information (Dummy Data), to services and Instantiated services

--- pages/services.tsx
+++ pages/services.tsx
@@ -2,11 +2,37 @@ import { NextPage } from "next";
 
 import { Page } from "../lib/components/layout/Page";
 import { ServicesTable } from "../lib/components/layout/tables/ServicesTable";
+import { useDescriptorUploadDialog } from "../lib/hooks/useDescriptorUploadDialog";
+import { Service } from "../lib/models/Service";
 
 const ServicesPage: NextPage = () => {
+  const showDescriptorUploadDialog = useDescriptorUploadDialog();
+
+  const data: Service[] = [
+    {
+      status: "active",
+      signature: "null",
+      md5: "832116ac25c690cab0b7caa3f5cc2fc4",
+      updated_at: new Date(),
+      created_at: new Date(),
+      username: "null",
+      uuid: "d8b045db-8276-45ad-bd32-bfba08da75a2",
+      cosd: {
+        descriptor_version: "1.0",
+        vendor: "eu.sonata-nfv.service-descriptor",
+        name: "ICMP-forwarder",
+        version: "1.0",
+        author: "Elton John",
+        description: "ICMP forwarder; consists of CN- and VM-based VNFs",
+        network_functions: "null",
+        cloud_services: "null",
+        forwarding_graphs: "null",
+      },
+    },
+  ];
   return (
     <Page title="Instantiated Services">
-      <ServicesTable></ServicesTable>
+      <ServicesTable data={data}></ServicesTable>
     </Page>
   );
 };
