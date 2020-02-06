b9da331a30b0988e183be958d78166dff2464ecd
Abdul Basit Tahir <abtcr7@gmail.com>
2020-02-06 16:47:21 +0100

    Updated Monitor graph to include HDD, CPU and RAM

--- lib/components/layout/charts/Monitoring.tsx
+++ lib/components/layout/charts/Monitoring.tsx
@@ -17,43 +17,43 @@ const data = [
     name: "Time A",
     CPU: 4000,
     RAM: 2400,
-    amt: 2400,
+    HDD: 2400,
   },
   {
     name: "Time B",
     CPU: 3000,
     RAM: 1398,
-    amt: 2210,
+    HDD: 2210,
   },
   {
     name: "Time C",
     CPU: 2000,
     RAM: 9800,
-    amt: 2290,
+    HDD: 2290,
   },
   {
     name: "Time D",
     CPU: 2780,
     RAM: 3908,
-    amt: 2000,
+    HDD: 2000,
   },
   {
     name: "Time E",
     CPU: 1890,
     RAM: 4800,
-    amt: 2181,
+    HDD: 2181,
   },
   {
     name: "Time F",
     CPU: 2390,
     RAM: 3800,
-    amt: 2500,
+    HDD: 2500,
   },
   {
     name: "Time G",
     CPU: 3490,
     RAM: 4300,
-    amt: 2100,
+    HDD: 2100,
   },
 ];
 
@@ -65,6 +65,10 @@ export default class MonitorGraph extends PureComponent {
       <ResponsiveContainer width="100%" height={300}>
         <AreaChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
           <defs>
+            <linearGradient id="colorHDD" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="15%" stopColor="#D62F0B" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#D62F0B" stopOpacity={0} />
+            </linearGradient>
             <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
               <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
               <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
@@ -78,6 +82,7 @@ export default class MonitorGraph extends PureComponent {
           <YAxis />
           <CartesianGrid strokeDasharray="3 3" />
           <Tooltip />
+          <Area type="monotone" dataKey="HDD" stroke="#000" fillOpacity={1} fill="url(#colorHDD)" />
           <Area
             type="monotone"
             dataKey="CPU"
