553265cd492ec33c705939c3a92942a6f123a12e
Abdul Basit Tahir <abtcr7@gmail.com>
2020-02-20 16:54:03 +0100

    Created a service instance table

---
+++ lib/components/layout/tables/ServiceInstancesTable.tsx
@@ -0,0 +1,80 @@
+import { IconButton } from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import { HighlightOff, Info as InfoIcon, PlayCircleOutline, StopRounded } from "@material-ui/icons";
+import React from "react";
+
+import { useServiceInfoDialog } from "../../../hooks/useServiceInfoDialog";
+import { useServiceStopDialog } from "../../../hooks/useServiceStopDialog";
+import { Service } from "../../../models/Service";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+type Props = {
+  /**
+   * Property to check page name
+   */
+  pageName?: any;
+  data: Service[];
+};
+
+export const ServiceInstancesTable: React.FunctionComponent<Props> = props => {
+  const classes = useStyles({});
+  const theme = useTheme();
+  const showServiceInfoDialog = useServiceInfoDialog();
+  const showServiceStopDialog = useServiceStopDialog();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="center" style={{ width: "20px" }}>
+              Version
+            </TableCell>
+            <TableCell align="center">Description</TableCell>
+            <TableCell align="center" style={{ width: "200px" }}>
+              Actions
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.data.map(row => (
+            <TableRow key={row.cosd.name}>
+              <TableCell component="th" scope="row">
+                {row.cosd.name}
+              </TableCell>
+              <TableCell align="left">{row.cosd.version}</TableCell>
+              <TableCell align="center">{row.cosd.description}</TableCell>
+              <TableCell align="center">
+                <IconButton color="primary" onClick={() => showServiceInfoDialog(row)}>
+                  <InfoIcon />
+                </IconButton>
+                <IconButton>
+                  <PlayCircleOutline htmlColor={theme.palette.success.main} />
+                </IconButton>
+                <IconButton
+                  color="primary"
+                  onClick={() => showServiceStopDialog(row.uuid, row.cosd.name)}
+                >
+                  <StopRounded htmlColor={theme.palette.error.main} />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
