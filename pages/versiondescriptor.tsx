bb9f6d8c449532ff09e729117224c1d8dd274531
Abdul Basit Tahir <abtcr7@gmail.com>
2020-02-20 16:58:56 +0100

    Version Desctriptor page added

---
+++ pages/versiondescriptor.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+import { Page } from "../lib/components/layout/Page";
+
+const versiondescriptor: NextPage = () => {
+  return <Page title="Version Descriptors">Coming Soon...</Page>;
+};
+
+export default versiondescriptor;
