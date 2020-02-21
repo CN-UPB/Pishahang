f2424dd63085a114b8618de8c3877caf7c878746
basit <basit@entangle09.cs.uni-paderborn.de>
2020-02-21 18:23:19 +0100

    Added Get and Delete service by UUID

--- src/gatekeeper/gatekeeper/api/services.py
+++ src/gatekeeper/gatekeeper/api/services.py
@@ -3,6 +3,23 @@ import requests
 from ..config import config
 catalogueRootUrl = config['urls']['catalogues'] + "complex-services"
 
+
 def getServices():
     headers = {'content-type': 'application/json'}
     return requests.get(catalogueRootUrl, headers=headers).json()
+
+
+def getServiceById(serviceId):
+    serviceIdUrl = catalogueRootUrl + "/" + serviceId
+    headers = {'content-type': 'application/json'}
+    response = requests.get(serviceIdUrl, headers=headers)
+    # Checking response
+    return response.json(), response.status_code
+
+
+def deleteServiceById(serviceId):
+    serviceIdUrl = catalogueRootUrl + "/" + serviceId
+    headers = {'content-type': 'application/json'}
+    response = requests.delete(serviceIdUrl, headers=headers)
+    # Checking response
+    return "Deleted", response.status_code
