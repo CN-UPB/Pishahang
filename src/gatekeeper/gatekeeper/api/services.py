c18a3f5aecc034c3827b60f184cfa7c4c1956521
bjoluc <mail@bjoluc.de>
2020-02-21 16:03:10 +0100

    Use complex services

--- src/gatekeeper/gatekeeper/api/services.py
+++ src/gatekeeper/gatekeeper/api/services.py
@@ -1,7 +1,7 @@
 import requests
 
 from ..config import config
-catalogueRootUrl = config['urls']['catalogues'] + "network-services"
+catalogueRootUrl = config['urls']['catalogues'] + "complex-services"
 
 def getServices():
     headers = {'content-type': 'application/json'}
