95bacc25a78ded7ecb0ab1d19490bd9f52ef55f6
bjoluc <mail@bjoluc.de>
2020-02-21 17:32:11 +0100

    Validate responses

--- src/gatekeeper/gatekeeper/__main__.py
+++ src/gatekeeper/gatekeeper/__main__.py
@@ -5,7 +5,7 @@ import connexion
 app = connexion.App(__name__, specification_dir='../')
 
 # Read the swagger.yml file to configure the endpoints
-app.add_api('swagger.yml')
+app.add_api('swagger.yml', validate_responses=True)
 
 # Create a URL route in our application for "/"
 @app.route('/')
