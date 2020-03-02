fc4f9ebc82f905a3ccb2ecc0be25afb539be24f9
basit <basit@entangle09.cs.uni-paderborn.de>
2020-03-02 19:46:23 +0100

    Updated Model to contain location of descriptor

--- src/gatekeeper/gatekeeper/models.py
+++ src/gatekeeper/gatekeeper/models.py
@@ -7,6 +7,7 @@ from uuid import uuid4
 from mongoengine import UUIDField, DictField, StringField
 from mongoengine_goodjson import Document
 
+
 class Descriptor(Document):
     """
     A mongoengine document base class for arbitrary descriptors
@@ -14,3 +15,5 @@ class Descriptor(Document):
     uuid = UUIDField(default=uuid4, primary_key=True)
     type = StringField(required=True)
     descriptor = DictField(required=True)
+    # setting file location Uploaded/Onboarded/Instantiated
+    location = StringField(required=True)
