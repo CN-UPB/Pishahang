db650fedcdac9a6162f377e87e654d0745d167a3
basit <basit@entangle09.cs.uni-paderborn.de>
2020-03-02 19:48:06 +0100

    dummy test

--- src/gatekeeper/gatekeeper/api/services_test.py
+++ src/gatekeeper/gatekeeper/api/services_test.py
@@ -1,8 +1,10 @@
 from .services import getServices, catalogueRootUrl
 
+
 def testGetServices(requests_mock):
     requests_mock.get(catalogueRootUrl, json=[])
     assert len(getServices()) == 0
 
-    requests_mock.get(catalogueRootUrl, json=["service1-dummy", "service2-dummy"])
+    requests_mock.get(catalogueRootUrl, json=[
+                      "service1-dummy", "service2-dummy"])
     assert len(getServices()) == 2
