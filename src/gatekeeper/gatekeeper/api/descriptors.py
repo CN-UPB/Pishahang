01b8e207ceb4dc616c38b77d9971a01ab8341313
bjoluc <mail@bjoluc.de>
2020-03-05 20:10:28 +0100

    Use document inheritance

--- src/gatekeeper/gatekeeper/api/descriptors.py
+++ src/gatekeeper/gatekeeper/api/descriptors.py
@@ -1,113 +1,58 @@
-from ..models import Descriptor
+from enum import Enum
+from typing import Type
 
-# returns a list of all uploaded descriptors
-
-
-def getDescriptors():
-    return Descriptor.objects(location="Uploaded")
+from ..models import Descriptor, OnboardedDescriptor, UploadedDescriptor
+from ..util import makeErrorResponse
 
 
-def getDescriptorsWithType(type):
-    return Descriptor.objects(type=type, location="Uploaded")
+class DescriptorType(Enum):
+    SERVICE = "service"
+    VM = "vm"
+    CN = "cn"
+    FPGA = "fpga"
 
 
-def getDescriptorWithUUID(uuid):
-    return Descriptor.objects(uuid=uuid, location="Uploaded")
+def getDescriptorsByType(descriptorClass: Type[Descriptor], type: DescriptorType):
+    """
+    Returns a QuerySet of descriptors of a specified Descriptor subclass and type.
+    """
+    return descriptorClass.objects(type=type)
 
-# adds a descriptor to the database
+
+def getUploadedDescriptorsByType(type: DescriptorType):
+    """
+    Returns a QuerySet of uploaded descriptors of a given type.
+    """
+    return getDescriptorsByType(UploadedDescriptor, type)
 
 
-def addDescriptor(type):
-    d = Descriptor(type=type, descriptor={
-                   'test': True}, location="Uploaded")
+def getOnboardedDescriptorsByType(type: DescriptorType):
+    """
+    Returns a QuerySet of onboarded descriptors of a given type.
+    """
+    return getDescriptorsByType(OnboardedDescriptor, type)
+
+
+def getDescriptorById(id):
+    """
+    Returns a given descriptor by its ID, or a 404 error if no descriptor matching the given id
+    exists.
+    """
+    descriptors = Descriptor.objects(uuid=id)
+    if(len(descriptors) != 0):
+        return descriptors.first()
+    return makeErrorResponse(404, "No descriptor matching the given id was found.")
+
+
+def addUploadedDescriptor(body):
+    d = UploadedDescriptor(**body)
     d.save()
-    return Descriptor.objects()
+    return d
 
 
-def addDescriptorInQuery(type, descriptor):
-    print("type")
-
-
-def addDescriptorWithType(descriptor):
-    print(descriptor)
-    # desc = Descriptor(type=type, descriptor=descriptor)
-    # desc.save()
-    # return Descriptor.objects()
-
-# deletes a descriptor from the database given a UUID
-
-
-def deleteDescriptorWithUUID(uuid):
-    return Descriptor.objects(uuid=uuid).delete()
-
-
-##ONBOARDED DESCRIPTORS##
-
-# returns a list of all onboarded descriptors
-
-
-def getOnboardedDescriptors():
-    return Descriptor.objects(location="Onboarded")
-    # returns a list of onboarded descriptors defined by type
-
-
-def getOnboardedTypeDescriptors(type):
-    return Descriptor.objects(type=type, location="Onboarded")
-    # returns a specific descriptor with a matching UUID
-
-
-def getOnboardedDescriptorWithUUID(uuid):
-    return Descriptor.objects(uuid=uuid, location="Onboarded")
-    # adds an onboarded descriptor to the database
-
-
-def addOnboardedDescriptor(type):
-    d = Descriptor(type=type, descriptor={
-                   'test': True}, location="Onboarded")
-    d.save()
-    return Descriptor.objects()
-    # deletes a decriptor given a UUID
-
-
-def deleteOnboardedDescriptor(uuid):
-    value = Descriptor.objects(uuid=uuid, location="Onboarded").delete()
-    if value == 1:
-        return "Deleted"
-    else:
-        return "Not Found"
-
-
-##INSTANTIATED DESCRIPTORS##
-
-# returns a list of all onboarded descriptors
-
-
-def getInstantiatedDescriptors():
-    return Descriptor.objects(location="Instantiated")
-    # returns a list of Instantiated descriptors defined by type
-
-
-def getInstantiatedDescriptorsWithType(type):
-    return Descriptor.objects(type=type, location="Instantiated")
-    # returns a specific descriptor with a matching UUID
-
-
-def getInstantiatedDescriptorsWithUUID(uuid):
-    return Descriptor.objects(uuid=uuid, location="Instantiated")
-    # adds an Instantiated descriptor to the database
-
-
-def addInstantiatedDescriptor(type):
-    d = Descriptor(type=type, descriptor={
-                   'test': True}, location="Instantiated")
-    d.save()
-    return Descriptor.objects()
-    # deletes a decriptor given a UUID
-
-
-def deleteInstantiatedDescriptor(uuid):
-    value = Descriptor.objects(uuid=uuid, location="Instantiated").delete()
-    if value == 1:
-        return "Deleted"
-    else:
-        return "Not Found"
+# def deleteOnboardedDescriptor(uuid):
+#     value = Descriptor.objects(uuid=uuid, location="Onboarded").delete()
+#     if value == 1:
+#         return "Deleted"
+#     else:
+#         return "Not Found"
