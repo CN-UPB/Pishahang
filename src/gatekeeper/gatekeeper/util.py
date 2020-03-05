69f7f771ac910a1fb68ce7af33d5c04bdcc5328e
bjoluc <mail@bjoluc.de>
2020-03-05 20:07:51 +0100

    Add convenience methods, improve serialization

--- src/gatekeeper/gatekeeper/util.py
+++ src/gatekeeper/gatekeeper/util.py
@@ -1,8 +1,8 @@
+import json
+
 from flask.json import JSONEncoder
 from mongoengine.base import BaseDocument
 from mongoengine.queryset import QuerySet
-
-import json
 
 
 class MongoEngineJSONEncoder(JSONEncoder):
@@ -12,8 +12,26 @@ class MongoEngineJSONEncoder(JSONEncoder):
     """
 
     def default(self, obj):
-        if isinstance(obj, BaseDocument) or isinstance(obj, QuerySet):
-            return json.loads(obj.to_json()) # TODO Replacement for the json.loads cheat?
+        if isinstance(obj, BaseDocument):
+            serializeableDict: dict = json.loads(obj.to_json())
+            if "_cls" in serializeableDict: # Remove Mongoengine class field if it exists
+                serializeableDict.pop("_cls")
+            return serializeableDict
+        if isinstance(obj, QuerySet):
+            return [self.default(entry) for entry in obj]
+
         return super().default(obj)
 
+def makeErrorDict(status: int, detail: str):
+    """
+    Given a `detail` string with a message and a `status` integer, returns a dictionary containing
+    those two items.
+    """
+    return {"detail": detail, "status": status}
 
+def makeErrorResponse(status: int, detail: str):
+    """
+    Given a `detail` string and a `status` integer, returns a tuple containing the result of
+    `makeErrorDict()` and the `status` code. This can be returned from flask route handlers.
+    """
+    return makeErrorDict(status, detail), status
