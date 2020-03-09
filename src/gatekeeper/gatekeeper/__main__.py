399be6cb5209634f1b73d6b418413c7cdc6ee0f6
bjoluc <mail@bjoluc.de>
2020-03-09 20:54:22 +0100

    Fix flask debug mode

--- src/gatekeeper/gatekeeper/__main__.py
+++ src/gatekeeper/gatekeeper/__main__.py
@@ -1,7 +1,9 @@
 from os import environ
 
+from config2.config import config
 from .app import app
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5555)
+    debug = config.get_env() == "development"
+    app.run(host='0.0.0.0', port=5555, debug=debug)
