8041e64788f8e0bf3ac73fc91aa51bb7675c4d74
Abdul Basit Tahir <abtcr7@gmail.com>
2020-03-09 20:26:27 +0100

    updated service modal

--- lib/models/Service.ts
+++ lib/models/Service.ts
@@ -1,17 +1,14 @@
 import { cosdService } from "./cosdService";
+import { DescriptorType } from "./descriptorType";
 
 /**
  * http://131.234.29.248/api/v2/complex-services?status=active&limit=10&offset=0
  * Complex Services
  */
 export interface Service {
-  created_at: Date;
-  md5: string;
-  signature: string;
-  /** status of service */
-  status: string;
-  updated_at: Date;
-  username: string;
-  uuid: string;
-  cosd: cosdService;
+  type: DescriptorType;
+  createdAt: string;
+  updatedAt: string;
+  id: string;
+  descriptor: cosdService;
 }
