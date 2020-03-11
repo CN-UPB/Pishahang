f606e6fd3d2b03812754fa66a1f5f876413a2a33
bjoluc <mail@bjoluc.de>
2020-03-11 15:46:46 +0100

    Add config API

--- src/gatekeeper/gatekeeper/app.py
+++ src/gatekeeper/gatekeeper/app.py
@@ -1,25 +1,32 @@
 from os import environ
 
 import connexion
+import redis
 from config2.config import config
-from flask import request
+from flask import request, g
 from flask_mongoengine import MongoEngine
+from flask_redis import FlaskRedis
 
 from .util import MongoEngineJSONEncoder
 
 # Create the application instance
-app = connexion.FlaskApp(__name__, specification_dir='../specification/')
-
-# Read the swagger.yml file to configure the endpoints
-app.add_api('openapi.yml', validate_responses=False)
+app = connexion.FlaskApp(__name__, specification_dir='../specification/', options=config.connexion)
 
 # Set flask environment, if ENV variable is set
 if config.get_env() is not None:
     app.app.config['ENV'] = config.get_env()
 
-# Add mongoengine database connection config
+# Setup mongoengine
 app.app.config['MONGODB_SETTINGS'] = {'host': config.databases.mongo}
 mongoDb = MongoEngine(app.app)
+
+# Setup redis database
+app.app.config['REDIS_URL'] = config.databases.redis
+redisClient: redis.Redis = FlaskRedis(app.app)
+
+# Read the specification files to configure the endpoints
+app.add_api('openapi.yml', validate_responses=False)
+app.add_api('config.yml')
 
 # Set a custom JSON encoder
 app.app.json_encoder = MongoEngineJSONEncoder
@@ -28,12 +35,3 @@ app.app.json_encoder = MongoEngineJSONEncoder
 @app.route('/')
 def home():
     return "It works!"
-
-# Pishahang-internal routes
-@app.route('/internal/keycloak-client-secret', methods=['POST'])
-def setKeycloakClientSecret():
-    """
-    Sets the Keycloak client secret used to authenticate the gatekeeper at the Keycloak API. It will
-    be posted by the Keycloak container on its initialization.
-    """
-    pass
