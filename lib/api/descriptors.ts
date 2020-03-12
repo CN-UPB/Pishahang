27c8b648112b1962109d913d222cb1f308b07098
Abdul Basit Tahir <abtcr7@gmail.com>
2020-03-12 20:45:38 +0100

    Modified descriptors api functions to be in a single file

--- lib/api/descriptors.ts
+++ lib/api/descriptors.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse, Method } from "axios";
 
 import { ApiReply } from "../models/ApiReply";
-import { DescriptorType } from "../models/descriptorType";
+import { Descriptor } from "../models/Descriptor";
+import { DescriptorType } from "../models/DescriptorType";
 import { RegisterUser } from "../models/RegisterUser";
 import { Session } from "../models/Session";
 import { ApiDataEndpoint, ApiDataEndpointReturnType } from "./endpoints";
@@ -24,11 +25,93 @@ export async function uploadDescriptor(
       descriptor: descriptorContent,
       type: descriptorType,
     });
+    window.location.reload(false);
     return { success: true };
   } catch (error) {
     switch ((error as AxiosError).response?.status) {
       case 401:
-        return { success: false, message: "Invalid username or password" };
+        return { success: false, message: "Invalid data" };
+      default:
+        return { success: false, message: "An unexpected error occurred. Please try again." };
+    }
+  }
+}
+
+/**
+ * Fetches a descriptor based on the given UUID
+ *
+ *
+ *
+ * @returns An `ApiReply` object with a user-friendly error message in case of failure.
+ */
+
+export async function getDescriptor(uuid: String): Promise<ApiReply> {
+  try {
+    if (uuid == null) return;
+    const reply = await axios.get(getApiUrl("uploaded-descriptors/" + uuid));
+    window.location.reload(false);
+    console.log(reply);
+
+    return { success: true };
+  } catch (error) {
+    switch ((error as AxiosError).response?.status) {
+      case 401:
+        return { success: false, message: "Invalid data" };
+      default:
+        return { success: false, message: "An unexpected error occurred. Please try again." };
+    }
+  }
+}
+
+/**
+ * Sends a deletion request to the API
+ *
+ *
+ *
+ * @returns An `ApiReply` object with a user-friendly error message in case of failure.
+ */
+
+export async function deleteDescriptor(uuid: String): Promise<ApiReply> {
+  try {
+    if (uuid == null) return;
+    const reply = await axios.delete(getApiUrl("uploaded-descriptors/" + uuid));
+    window.location.reload(false);
+    console.log(uuid);
+
+    return { success: true };
+  } catch (error) {
+    switch ((error as AxiosError).response?.status) {
+      case 401:
+        return { success: false, message: "Invalid data" };
+      default:
+        return { success: false, message: "An unexpected error occurred. Please try again." };
+    }
+  }
+}
+
+/**
+ * Updates a descriptor with the given uuid
+ *
+ *
+ *
+ * @returns An `ApiReply` object with a user-friendly error message in case of failure.
+ */
+
+export async function updateDescriptor(
+  descriptorContent: Descriptor,
+  uuid: String
+): Promise<ApiReply> {
+  try {
+    const reply = await axios.put(getApiUrl("uploaded-descriptors/" + uuid), {
+      descriptor: descriptorContent,
+    });
+
+    window.location.reload(false);
+    return { success: true };
+  } catch (error) {
+    switch ((error as AxiosError).response?.status) {
+      case 401:
+        return { success: false, message: "Invalid data" };
       default:
         return { success: false, message: "An unexpected error occurred. Please try again." };
     }
