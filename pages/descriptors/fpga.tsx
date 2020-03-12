789c8137858145127e2d3a4e6028627982f04c57
Abdul Basit Tahir <abtcr7@gmail.com>
2020-03-12 16:54:27 +0100

    Added FPGA to work with descritor uploading and showing.

--- pages/descriptors/fpga.tsx
+++ pages/descriptors/fpga.tsx
@@ -1,9 +1,15 @@
 import { NextPage } from "next";
 
 import { Page } from "../../lib/components/layout/Page";
+import { DescriptorType } from "../../lib/models/descriptorType";
+import { DescriptorPageContent } from "./DescriptorPageContent";
 
 const ContainersPage: NextPage = () => {
-  return <Page title="FPGA Based VNF Descriptors">Coming Soon...</Page>;
+  return (
+    <Page title="FPGA Based VNF Descriptors">
+      <DescriptorPageContent type={DescriptorType.FPGA}></DescriptorPageContent>
+    </Page>
+  );
 };
 
 export default ContainersPage;
