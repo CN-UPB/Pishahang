42201726a281954943b63d7c141ad1c3f7f80f21
bjoluc <mail@bjoluc.de>
2020-04-01 11:12:59 +0200

    Fix descriptor API handlers

--- lib/api/descriptors.ts
+++ lib/api/descriptors.ts
@@ -1,5 +1,6 @@
-import axios, { AxiosError, AxiosRequestConfig, AxiosResponse, Method } from "axios";
+import axios, { AxiosError } from "axios";
 
+import { Descriptor } from "./../models/Descriptor";
 import { ApiReply } from "../models/ApiReply";
 import { DescriptorContent } from "../models/Descriptor";
 import { DescriptorType } from "../models/Descriptor";
@@ -9,19 +10,18 @@ import { getApiUrl } from "./index";
  * Sends a registration request to the API
  *
  * @returns An `ApiReply` object with a user-friendly error message in case of failure. In case of
- * success, the resulting `Session` object is provided via the `payload` attribute.
+ * success, the resulting `Descriptor` object is provided via the `payload` attribute.
  */
 export async function uploadDescriptor(
-  descriptorType: DescriptorType,
-  descriptorContent: any
-): Promise<ApiReply> {
+  type: DescriptorType,
+  content: any
+): Promise<ApiReply<Descriptor>> {
   try {
     const reply = await axios.post(getApiUrl("descriptors"), {
-      descriptor: descriptorContent,
-      type: descriptorType,
+      descriptor: content,
+      type: type,
     });
-    window.location.reload(false);
-    return { success: true };
+    return { success: true, payload: reply.data };
   } catch (error) {
     switch ((error as AxiosError).response?.status) {
       case 401:
@@ -44,11 +44,8 @@ export async function getDescriptor(uuid: String): Promise<ApiReply> {
   try {
     if (uuid == null) return;
     const reply = await axios.get(getApiUrl("descriptors/" + uuid));
-    window.location.reload(false);
-
     console.log(reply);
-
-    return { success: true };
+    return { success: true, payload: reply.data };
   } catch (error) {
     switch ((error as AxiosError).response?.status) {
       case 401:
@@ -70,8 +67,7 @@ export async function getDescriptor(uuid: String): Promise<ApiReply> {
 export async function deleteDescriptor(uuid: String): Promise<ApiReply> {
   try {
     if (uuid == null) return;
-    const reply = await axios.delete(getApiUrl("descriptors/" + uuid));
-    window.location.reload(false);
+    await axios.delete(getApiUrl("descriptors/" + uuid));
     console.log(uuid);
 
     return { success: true };
@@ -94,15 +90,11 @@ export async function deleteDescriptor(uuid: String): Promise<ApiReply> {
  */
 
 export async function updateDescriptor(
-  descriptorContent: DescriptorContent,
+  content: DescriptorContent,
   uuid: String
 ): Promise<ApiReply> {
   try {
-    const reply = await axios.put(getApiUrl("descriptors/" + uuid), {
-      descriptor: descriptorContent,
-    });
-
-    window.location.reload(false);
+    await axios.put(getApiUrl("descriptors/" + uuid), { content });
     return { success: true };
   } catch (error) {
     switch ((error as AxiosError).response?.status) {
