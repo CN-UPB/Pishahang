2c048dd9c51eeb53351ccd4bffdbec0aef553789
Abdul Basit Tahir <abtcr7@gmail.com>
2020-04-06 19:24:07 +0200

    Renamed Global to Dialogs

--- lib/store/selectors/global.ts
+++
@@ -1,4 +0,0 @@
-import { RootState } from "StoreTypes";
-
-export const selectSnackbarMessage = (state: RootState) => state.global.snackbar.message;
-export const selectSnackbarIsVisible = (state: RootState) => state.global.snackbar.isVisible;
