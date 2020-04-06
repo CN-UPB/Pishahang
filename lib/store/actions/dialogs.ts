ce366b9d5a20b0754597ba49a1981c366db9601b
Abdul Basit Tahir <abtcr7@gmail.com>
2020-04-06 19:13:47 +0200

    Updated dialoges to hold title and message. Co- Authored : Björn

--- lib/store/actions/dialogs.ts
+++ lib/store/actions/dialogs.ts
@@ -3,5 +3,8 @@ import { createAction } from "typesafe-actions";
 export const showSnackbar = createAction("Global:Snackbar:show")<string>();
 export const resetSnackbar = createAction("Global:Snackbar:reset")();
 
-export const showErrorInfoDialog = createAction("Global:ErrorInformationDialog:show")<string>();
-export const resetInfoDialog = createAction("Global:ErrorInformationDialog:reset")();
+export const showInfoDialog = createAction("Global:InfoDialog:show")<{
+  title: string;
+  message: string;
+}>();
+export const resetInfoDialog = createAction("Global:InfoDialog:reset")();
