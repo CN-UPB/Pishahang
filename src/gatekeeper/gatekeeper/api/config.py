fcd6efa8dcfd626524b0a1fa8e710469d1315772
bjoluc <mail@bjoluc.de>
2020-04-10 16:30:50 +0200

    Remove outdated config API module

--- src/gatekeeper/gatekeeper/api/config.py
+++
@@ -1,8 +0,0 @@
-from ..app import redisClient
-from ..util import makeMessageResponse
-
-
-def setKeycloakClientSecret(body: bytes):
-    secret = body.decode("utf-8")
-    redisClient.set('keycloakClientSecret', secret)
-    return makeMessageResponse(200, "Secret successfully set")
