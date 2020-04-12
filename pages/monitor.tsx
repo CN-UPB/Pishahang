a7ea01e413749c5195ff67651b172cf901ee5fc7
Abdul Basit Tahir <abtcr7@gmail.com>
2020-04-12 14:39:40 +0200

    Updated Monitoring page to display mano plugins Information

--- pages/monitor.tsx
+++ pages/monitor.tsx
@@ -1,14 +1,27 @@
+import axios from "axios";
 import { NextPage } from "next";
+import useSWR from "swr";
 
+import { getApiUrl } from "../lib/api";
 import MonitorGraph from "../lib/components/layout/charts/Monitoring";
 import { Page } from "../lib/components/layout/Page";
+import { PluginsTable } from "../lib/components/layout/tables/PluginsTable";
 
 const MonitoringPage: NextPage = () => {
-  return (
-    <Page title="Monitoring">
-      <MonitorGraph></MonitorGraph>
-    </Page>
-  );
+  const { data, error } = useSWR(getApiUrl("plugins"), axios.get);
+  if (!data || error) {
+    return (
+      <Page title="Monitoring">
+        <MonitorGraph></MonitorGraph>
+      </Page>
+    );
+  } else {
+    return (
+      <Page title="Monitoring">
+        <PluginsTable data={data.data}></PluginsTable>
+      </Page>
+    );
+  }
 };
 
 export default MonitoringPage;
