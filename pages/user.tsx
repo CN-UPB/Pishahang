53e21d29027d5a44f9f6ddbe5b575bd219888fd1
Abdul Basit Tahir <abtcr7@gmail.com>
2020-04-13 14:02:13 +0200

    Updated users, to accomodate for the new hook.

--- pages/user.tsx
+++ pages/user.tsx
@@ -1,12 +1,25 @@
+import { Fab, Tooltip } from "@material-ui/core";
+import { Add } from "@material-ui/icons";
 import { NextPage } from "next";
 
-import { UserProfile } from "./../lib/components/forms/userprofile";
 import { Page } from "../lib/components/layout/Page";
+import { useAddUserDialog } from "../lib/hooks/useAddUserDialog";
 
 const DashboardPage: NextPage = () => {
+  const showUserProfileDialog = useAddUserDialog();
   return (
     <Page title="User Profile">
-      <UserProfile />
+      <Tooltip title="Add User" arrow>
+        <Fab
+          color="primary"
+          size="small"
+          style={{ float: "right" }}
+          aria-label="Add"
+          onClick={() => showUserProfileDialog()}
+        >
+          <Add />
+        </Fab>
+      </Tooltip>
     </Page>
   );
 };
