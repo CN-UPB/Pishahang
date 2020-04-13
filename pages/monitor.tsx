8f6b2752bc5baedd0eceb43a4855b316776f07ff
Abdul Basit Tahir <abtcr7@gmail.com>
2020-04-13 18:41:52 +0200

    Refactored page code.
    Co-authored-by: bjoluc <mail@bjoluc.de>

--- pages/monitor.tsx
+++ pages/monitor.tsx
@@ -8,20 +8,11 @@ import { Page } from "../lib/components/layout/Page";
 import { PluginsTable } from "../lib/components/layout/tables/PluginsTable";
 
 const MonitoringPage: NextPage = () => {
-  const { data, error } = useSWR(getApiUrl("plugins"), axios.get);
-  if (!data || error) {
-    return (
-      <Page title="Monitoring">
-        <MonitorGraph></MonitorGraph>
-      </Page>
-    );
-  } else {
-    return (
-      <Page title="Monitoring">
-        <PluginsTable data={data.data}></PluginsTable>
-      </Page>
-    );
-  }
+  return (
+    <Page title="Monitoring">
+      <PluginsTable></PluginsTable>
+    </Page>
+  );
 };
 
 export default MonitoringPage;
