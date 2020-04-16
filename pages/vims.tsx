b61af54ded5203afa3384c4f5a28f37078d40a53
Abdul Basit Tahir <abtcr7@gmail.com>
2020-04-16 20:32:23 +0200

    Refactored VIMs page.

--- pages/vims.tsx
+++ pages/vims.tsx
@@ -12,58 +12,22 @@ import { Vim, VimType } from "../lib/models/Vims";
 
 const VimPage: NextPage = () => {
   const showVimDialog = useVimsCreaterDialog();
-
-  //Add fetch request
-  const { data, error } = useSWR(getApiUrl("vims"), axios.get);
-
-  const defaultData: Vim[] = [
-    {
-      vimName: "OpenStack",
-      vimType: VimType.OpenStack,
-      country: "DE",
-      city: "PB",
-      uuid: "f57d7590-3652-43e0-b231-6ad49e602e50",
-      vendor: "Pishahang",
-      cores: "2",
-      memory: "1.2 gb",
-    },
-  ];
-
-  if (!data || error) {
-    return (
-      <Page title="VIM Settings">
-        <Tooltip title="Add VIM" arrow>
-          <Fab
-            color="primary"
-            size="small"
-            style={{ float: "right" }}
-            aria-label="Upload"
-            onClick={showVimDialog}
-          >
-            <Add />
-          </Fab>
-        </Tooltip>
-        <VimsTable data={defaultData}></VimsTable>
-      </Page>
-    );
-  } else {
-    return (
-      <Page title="VIM Settings">
-        <Tooltip title="Add VIM" arrow>
-          <Fab
-            color="primary"
-            size="small"
-            style={{ float: "right" }}
-            aria-label="Upload"
-            onClick={showVimDialog}
-          >
-            <Add />
-          </Fab>
-        </Tooltip>
-        <VimsTable data={data.data}></VimsTable>
-      </Page>
-    );
-  }
+  return (
+    <Page title="VIM Settings">
+      <Tooltip title="Add VIM" arrow>
+        <Fab
+          color="primary"
+          size="small"
+          style={{ float: "right" }}
+          aria-label="Upload"
+          onClick={showVimDialog}
+        >
+          <Add />
+        </Fab>
+      </Tooltip>
+      <VimsTable></VimsTable>
+    </Page>
+  );
 };
 
 export default VimPage;
