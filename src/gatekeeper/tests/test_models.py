f68f10393644bd320ace2dd4eb3508cf35e3f14a
bjoluc <mail@bjoluc.de>
2020-04-16 13:13:54 +0200

    Update user model unit test

--- src/gatekeeper/tests/test_models.py
+++ src/gatekeeper/tests/test_models.py
@@ -2,7 +2,13 @@ from gatekeeper.models.users import User
 
 
 def testUser():
-    User(username="bill", password="gates", isAdmin="true").save()
+    User(
+        username="bill",
+        password="gates",
+        isAdmin="true",
+        email="user@example.org",
+        fullName="Bill"
+    ).save()
 
     user: User = User.objects(username="bill").get()
     assert user.validatePassword("gates") is True
