bae443cfdfdae48349d85cd80ec818b8538f0c22
Abdul Basit Tahir <abtcr7@gmail.com>
2020-04-16 20:31:00 +0200

    Updated Vims info dialog according to new VIMs model

--- lib/hooks/useVimsInfoDialog.tsx
+++ lib/hooks/useVimsInfoDialog.tsx
@@ -12,7 +12,6 @@ import * as React from "react";
 import { useModal } from "react-modal-hook";
 
 import { GenericDialog } from "../components/layout/dialogs/GenericDialog";
-import { Descriptor } from "../models/Descriptor";
 import { Vim } from "../models/Vims";
 import { useStateRef } from "./useStateRef";
 
@@ -37,31 +36,31 @@ export function useVimsInfoDialog() {
       <TableContainer component={Paper}>
         <Table aria-label="simple table">
           <TableBody>
-            <TableRow key={dataRef.current.vimType}>
+            <TableRow key={dataRef.current.vimDomain}>
               <TableCell component="th" scope="row">
                 <Typography variant="body2" gutterBottom>
-                  VimType:
+                  Vim Domain:
                 </Typography>
               </TableCell>
-              <TableCell align="left">{dataRef.current.vimType}</TableCell>
+              <TableCell align="left">{dataRef.current.vimDomain}</TableCell>
             </TableRow>
 
-            <TableRow key={dataRef.current.city}>
+            <TableRow key={dataRef.current.vimCity}>
               <TableCell component="th" scope="row">
                 <Typography variant="body2" gutterBottom>
-                  City:
+                  Vim City:
                 </Typography>
               </TableCell>
-              <TableCell align="left">{dataRef.current.city}</TableCell>
+              <TableCell align="left">{dataRef.current.vimCity}</TableCell>
             </TableRow>
 
-            <TableRow key={dataRef.current.country}>
+            <TableRow key={dataRef.current.vimEndpoint}>
               <TableCell component="th" scope="row">
                 <Typography variant="body2" gutterBottom>
-                  Country:
+                  Vim Endpoint:
                 </Typography>
               </TableCell>
-              <TableCell align="left">{dataRef.current.country}</TableCell>
+              <TableCell align="left">{dataRef.current.vimEndpoint}</TableCell>
             </TableRow>
           </TableBody>
         </Table>
