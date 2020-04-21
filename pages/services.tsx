19e645df16736a4c0fb6dd78063a7ec61d4ca923
Abdul Basit Tahir <abtcr7@gmail.com>
2020-04-21 19:56:59 +0200

    Refactored services page

--- pages/services.tsx
+++ pages/services.tsx
@@ -1,25 +1,14 @@
 import { NextPage } from "next";
 
-import { ApiDataEndpoint } from "../lib/api/endpoints";
 import { ServicesTable } from "../lib/components/content/tables/ServicesTable";
 import { Page } from "../lib/components/layout/Page";
-import { useAuthorizedSWR } from "../lib/hooks/useAuthorizedSWR";
-import { useDescriptorUploadDialog } from "../lib/hooks/useDescriptorUploadDialog";
-import { DescriptorType } from "../lib/models/Descriptor";
 
 const ServicesPage: NextPage = () => {
-  const showDescriptorUploadDialog = useDescriptorUploadDialog(DescriptorType.Service);
-  const { data, error } = useAuthorizedSWR(ApiDataEndpoint.Services);
-
-  if (!data || error) {
-    return <Page title="Services">Error...</Page>;
-  } else {
-    return (
-      <Page title="Services">
-        <ServicesTable data={data}></ServicesTable>
-      </Page>
-    );
-  }
+  return (
+    <Page title="Services">
+      <ServicesTable></ServicesTable>
+    </Page>
+  );
 };
 
 export default ServicesPage;
