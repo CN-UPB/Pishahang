505075b17044af4a161db8a32a8c4122a102d78e
Ashwin <ashwinprasad.me@gmail.com>
2020-04-25 21:14:43 +0200

    [Feature] Traffic dataset from SIX [Feature] Policy generator and scatter plot [Fix] TFP build fix

--- son-mano-framework/plugins/son-mano-traffic-forecast/build_run_container.sh
+++ son-mano-framework/plugins/son-mano-traffic-forecast/build_run_container.sh
@@ -6,11 +6,11 @@ echo "$dir"
 
 cd ../..
 
-sudo docker stop tfplugin
-sudo docker rm tfplugin
-sudo docker build -t tfplugin -f plugins/son-mano-traffic-forecast/Dockerfile-dev .
+sudo docker stop pishahang_tfplugin_1
+sudo docker rm pishahang_tfplugin_1
 
-sudo docker run -d --name tfplugin --net=son-sp --network-alias=tfplugin -v $(pwd)/plugins/son-mano-traffic-forecast:/plugins/son-mano-traffic-forecast -p 8088:8888 -e USERID=1000 tfplugin
-# sudo docker run -d -i -t --name tfplugin --net=son-sp --network-alias=tfplugin -v $(pwd)/plugins/son-mano-traffic-forecast:/plugins/son-mano-traffic-forecast -p 8088:8888 -e USERID=1000 tfplugin jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --no-browser --NotebookApp.token='password' --notebook-dir=/plugins/son-mano-traffic-forecast/notebooks
+sudo docker build -t pishahang_tfplugin_1 -f plugins/son-mano-traffic-forecast/Dockerfile-dev .
+sudo docker run -d --name pishahang_tfplugin_1 --net=pishahang_pishanet --network-alias=tfplugin -v $(pwd)/plugins/son-mano-traffic-forecast:/plugins/son-mano-traffic-forecast -p 8088:8888 -e USERID=1000 pishahang_tfplugin_1
 
-sudo docker logs tfplugin -f
+# sudo docker logs pishahang_tfplugin_1 -f
+sudo docker exec -it pishahang_tfplugin_1 bash
