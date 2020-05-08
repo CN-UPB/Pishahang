707c52a3777363a8fd5b6cd50e848d0495ace862
bjoluc <mail@bjoluc.de>
2020-05-08 13:20:53 +0200

    Setup new VIM Adaptor project

---
+++ mano-framework/plugins/vim-adaptor/README.md
@@ -0,0 +1,8 @@
+# VIM Adaptor
+
+The VIM Adaptor is the bridge between Pishahang and a number of Virtual Infrastructure Managers (VIMs).
+It allows to deploy services in the following domains:
+
+* OpenStack
+* Kubernetes
+* AWS
---
+++ mano-framework/plugins/vim-adaptor/vim_adaptor/main.py
@@ -0,0 +1,55 @@
+"""
+Copyright (c) 2017 Pishahang
+ALL RIGHTS RESERVED.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Neither the name of Pishahang, nor the names of its contributors
+may be used to endorse or promote products derived from this software
+without specific prior written permission.
+"""
+
+import logging
+
+from manobase.messaging import Message
+from manobase.plugin import ManoBasePlugin
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("manobase:plugin").setLevel(logging.INFO)
+logging.getLogger("amqpstorm.channel").setLevel(logging.ERROR)
+
+LOG = logging.getLogger("plugin:vim-adaptor")
+
+
+class VimAdaptor(ManoBasePlugin):
+    """
+    Vim Adaptor main class. Instantiate this class to run the Vim Adaptor.
+    """
+
+    def __init__(self):
+        super().__init__(version="0.1.0")
+
+    def declare_subscriptions(self):
+        super().declare_subscriptions()
+
+    def on_lifecycle_start(self, message: Message):
+        super().on_lifecycle_start(message)
+        LOG.info("VIM Adaptor started.")
+
+
+def main():
+    VimAdaptor()
+
+
+if __name__ == "__main__":
+    main()
