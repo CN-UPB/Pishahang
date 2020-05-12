5bcffea5f22cf7fc0fbae0538f6033f6eef6ee84
bjoluc <mail@bjoluc.de>
2020-05-12 21:44:17 +0200

    Use AsyncioBrokerConnection in ManoBasePlugin

--- mano-framework/base/manobase/plugin.py
+++ mano-framework/base/manobase/plugin.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2015 SONATA-NFV
+Copyright (c) 2015 SONATA-NFV, 2017 Pishahang
 ALL RIGHTS RESERVED.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,12 +14,12 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-Neither the name of the SONATA-NFV [, ANY ADDITIONAL AFFILIATION]
+Neither the name of the SONATA-NFV, Pishahang,
 nor the names of its contributors may be used to endorse or promote
 products derived from this software without specific prior written
 permission.
 
-This work has been performed in the framework of the SONATA project,
+Parts of this work have been performed in the framework of the SONATA project,
 funded by the European Commission under Grant number 671517 through
 the Horizon 2020 and 5G-PPP programmes. The authors would like to
 acknowledge the contributions of their colleagues of the SONATA
@@ -28,12 +28,12 @@ partner consortium (www.sonata-nfv.eu).
 
 import logging
 import os
-import threading
 import time
+from threading import Event, Thread
 
-import amqpstorm
+from amqpstorm import AMQPConnectionError
 
-from manobase import messaging
+from manobase.messaging import AsyncioBrokerConnection, Message
 
 logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger("manobase:plugin")
@@ -76,32 +76,34 @@ class ManoBasePlugin(object):
         self.name = "%s.%s" % (name, self.__class__.__name__)
         self.version = version
         self.description = description
-        self.uuid = None  # uuid given by plugin manager on registration
-        self.state = None  # the state of this plugin READY/RUNNING/PAUSED/FAILED
+        self.uuid: str = None  # uuid given by plugin manager on registration
+        self.state: str = None  # the state of this plugin READY/RUNNING/PAUSED/FAILED
 
-        self._registered_event = threading.Event()
+        self._registered_event = Event()
 
         LOG.info("Starting MANO Plugin: %s ...", self.name)
         # create and initialize broker connection
         while True:
             try:
-                self.manoconn = messaging.ManoBrokerRequestResponseConnection(self.name)
+                self.conn = AsyncioBrokerConnection(self.name)
                 break
-            except amqpstorm.AMQPConnectionError:
+            except AMQPConnectionError:
                 time.sleep(5)
-        # register subscriptions
+        self.manoconn = self.conn  # For backwards compatibility
+
         LOG.info("Plugin connected to broker.")
 
         self.declare_subscriptions()
-        # register to plugin manager
+
+        # Register at plugin manager
         if auto_register:
-            while self.uuid is None:
-                self.register()
-                if wait_for_registration:
-                    self._wait_for_registration()
-        # kick-off automatic heartbeat mechanism
-        self._auto_heartbeat(auto_heartbeat_rate)
-        # jump to run
+            self.register()
+            if wait_for_registration:
+                self._wait_for_registration()
+
+        # Start hearbeat mechanism
+        self._start_heartbeats(auto_heartbeat_rate)
+
         if start_running:
             self.run()
 
@@ -112,11 +114,9 @@ class ManoBasePlugin(object):
         """
         # de-register this plugin
         self.deregister()
-        self.manoconn.stop_connection()
-        self.manoconn.stop_threads()
-        del self.manoconn
+        self.conn.close()
 
-    def _auto_heartbeat(self, rate):
+    def _start_heartbeats(self, rate):
         """
         A simple periodic heartbeat mechanism.
         (much room for improvements here)
@@ -132,13 +132,10 @@ class ManoBasePlugin(object):
                     self._send_heartbeat()
                 time.sleep(1 / rate)
 
-        # run heartbeats in separated thread
-        t = threading.Thread(target=run)
-        t.daemon = True
-        t.start()
+        Thread(target=run, daemon=True).start()
 
     def _send_heartbeat(self):
-        self.manoconn.notify(
+        self.conn.notify(
             "platform.management.plugin.%s.heartbeat" % str(self.uuid),
             {"uuid": self.uuid, "state": str(self.state)},
         )
@@ -149,7 +146,7 @@ class ManoBasePlugin(object):
         But: The this superclass method should be called in any case.
         """
         # plugin status update subscription
-        self.manoconn.register_notification_endpoint(
+        self.conn.register_notification_endpoint(
             self.on_plugin_status_update,  # call back method
             "platform.management.plugin.status",
         )
@@ -162,21 +159,21 @@ class ManoBasePlugin(object):
         while True:
             time.sleep(1)
 
-    def on_lifecycle_start(self, message: messaging.Message):
+    def on_lifecycle_start(self, message: Message):
         """
         To be overwritten by subclass
         """
         LOG.debug("Received lifecycle.start event.")
         self.state = "RUNNING"
 
-    def on_lifecycle_pause(self, message: messaging.Message):
+    def on_lifecycle_pause(self, message: Message):
         """
         To be overwritten by subclass
         """
         LOG.debug("Received lifecycle.pause event.")
         self.state = "PAUSED"
 
-    def on_lifecycle_stop(self, message: messaging.Message):
+    def on_lifecycle_stop(self, message: Message):
         """
         To be overwritten by subclass
         """
@@ -191,7 +188,7 @@ class ManoBasePlugin(object):
         LOG.debug("Received registration ok event.")
         pass
 
-    def on_plugin_status_update(self, message: messaging.Message):
+    def on_plugin_status_update(self, message: Message):
         """
         To be overwritten by subclass.
         Called when a plugin list status update
@@ -201,59 +198,50 @@ class ManoBasePlugin(object):
 
     def register(self):
         """
-        Send a register request to the plugin manager component to announce this plugin.
+        Register this plugin at the plugin manager.
         """
-        self.manoconn.call_async(
-            self._on_register_response,
-            "platform.management.plugin.register",
-            {
-                "name": self.name,
-                "version": self.version,
-                "description": self.description,
-            },
-        )
+        self.conn.run_coroutine(self._register())
 
-    def _on_register_response(self, message: messaging.Message):
-        """
-        Event triggered when register response is received.
-        :param props: response properties
-        :param response: response body
-        :return: None
-        """
-        response = message.payload
+    async def _register(self):
+        response = (
+            await self.conn.call(
+                "platform.management.plugin.register",
+                {
+                    "name": self.name,
+                    "version": self.version,
+                    "description": self.description,
+                },
+            )
+        ).payload
+
         if response["status"] != "OK":
             LOG.debug("Response %r", response)
             LOG.error("Plugin registration failed. Exit.")
             exit(1)
+
         self.uuid = response["uuid"]
-        # mark this plugin to be ready to be started
         self.state = "READY"
+
         LOG.info("Plugin registered with UUID: %s", response["uuid"])
-        # jump to on_registration_ok()
         self.on_registration_ok()
-        # subscribe to start topic
+
         self._register_lifecycle_endpoints()
         self._registered_event.set()
 
     def deregister(self):
         """
-        Send a deregister event to the plugin manager component.
+        De-registers the plugin at the plugin manager.
         """
+        self.conn.run_coroutine(self._deregister())
+
+    async def _deregister(self):
         LOG.info("De-registering plugin...")
-        self.manoconn.call_async(
-            self._on_deregister_response,
-            "platform.management.plugin.deregister",
-            {"uuid": self.uuid},
+
+        response = await self.conn.call(
+            "platform.management.plugin.deregister", {"uuid": self.uuid},
         )
 
-    def _on_deregister_response(self, message: messaging.Message):
-        """
-        Event triggered when de-register response is received.
-        :param props: response properties
-        :param response: response body
-        :return: None
-        """
-        if message.payload["status"] != "OK":
+        if response.payload["status"] != "OK":
             LOG.error("Plugin de-registration failed. Exit.")
             exit(1)
         self.uuid = None
@@ -271,18 +259,17 @@ class ManoBasePlugin(object):
 
     def _register_lifecycle_endpoints(self):
         if self.uuid is not None:
+            base_topic = "platform.management.plugin.%s.lifecycle." % str(self.uuid)
+
             # lifecycle.start
-            self.manoconn.register_notification_endpoint(
-                self.on_lifecycle_start,  # call back method
-                "platform.management.plugin.%s.lifecycle.start" % str(self.uuid),
+            self.conn.register_notification_endpoint(
+                self.on_lifecycle_start, base_topic + "start",
             )
             # lifecycle.pause
-            self.manoconn.register_notification_endpoint(
-                self.on_lifecycle_pause,  # call back method
-                "platform.management.plugin.%s.lifecycle.pause" % str(self.uuid),
+            self.conn.register_notification_endpoint(
+                self.on_lifecycle_pause, base_topic + "pause",
             )
             # lifecycle.stop
-            self.manoconn.register_notification_endpoint(
-                self.on_lifecycle_stop,  # call back method
-                "platform.management.plugin.%s.lifecycle.stop" % str(self.uuid),
+            self.conn.register_notification_endpoint(
+                self.on_lifecycle_stop, base_topic + "stop",
             )
