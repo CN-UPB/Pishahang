279f75fbad66ef0469864cd49f48ff5c7849def4
basit <abtcr7@gmail.com>
2020-05-25 19:40:41 +0200

    Updated to use a single uuid

--- mano-framework/plugins/vim-adaptor/vim_adaptor/managers/templates/kubernetes/cloud_service.tf
+++ mano-framework/plugins/vim-adaptor/vim_adaptor/managers/templates/kubernetes/cloud_service.tf
@@ -1,7 +1,7 @@
 {% for vdu in descriptor.virtual_deployment_units %}
-resource "kubernetes_replication_controller" "{{ vdu.id }}-{{ vdu.instance_uuid }}" {
+resource "kubernetes_replication_controller" "{{ vdu.id }}-{{ instance_uuid }}" {
   metadata {
-    name = "{{ vdu.id }}-{{ vdu.instance_uuid }}"
+    name = "{{ vdu.id }}-{{ instance_uuid }}"
     labels {
       service = "{{ service_instance_id  }}"
       vdu = "{{ vdu.id }}"
@@ -57,12 +57,12 @@ resource "kubernetes_replication_controller" "{{ vdu.id }}-{{ vdu.instance_uuid 
   }
 }
 
-resource "kubernetes_service" "{{ vdu.id }}-{{ vdu.instance_uuid }}" {
+resource "kubernetes_service" "{{ vdu.id }}-{{ instance_uuid }}" {
   metadata {
     {% if vdu.name != null %}
       name = "{{ vdu.name }}"
     {% else %}
-      name = "{{ vdu.id }}-{{ vdu.instance_uuid }}"
+      name = "{{ vdu.id }}-{{ instance_uuid }}"
     {% endif %}
 
     labels {
@@ -72,7 +72,7 @@ resource "kubernetes_service" "{{ vdu.id }}-{{ vdu.instance_uuid }}" {
   }
   spec {
     selector {
-      vdu = "${kubernetes_replication_controller.{{ vdu.id }}-{{ vdu.instance_uuid }}.metadata.0.labels.vdu}"
+      vdu = "${kubernetes_replication_controller.{{ vdu.id }}-{{ instance_uuid }}.metadata.0.labels.vdu}"
     }
 
     {% for port in vdu.service_ports %}
@@ -93,9 +93,9 @@ resource "kubernetes_service" "{{ vdu.id }}-{{ vdu.instance_uuid }}" {
 }
 
     {% if vdu.scale_in_out != null %}
-        resource "kubernetes_horizontal_pod_autoscaler" "{{ vdu.id }}-{{ vdu.instance_uuid }}" {
+        resource "kubernetes_horizontal_pod_autoscaler" "{{ vdu.id }}-{{ instance_uuid }}" {
           metadata {
-              name = "{{ vdu.id }}-{{ vdu.instance_uuid }}"
+              name = "{{ vdu.id }}-{{ instance_uuid }}"
 
               labels {
               service = "{{ service_instance_id  }}"
@@ -107,7 +107,7 @@ resource "kubernetes_service" "{{ vdu.id }}-{{ vdu.instance_uuid }}" {
               min_replicas = {{ vdu.scale_in_out.minimum }}
               scale_target_ref {
               kind = "ReplicationController"
-              name = "${kubernetes_replication_controller.{{ vdu.id }}-{{ vdu.instance_uuid }}.metadata.0.name}"
+              name = "${kubernetes_replication_controller.{{ vdu.id }}-{{ instance_uuid }}.metadata.0.name}"
               }
           }
         }
