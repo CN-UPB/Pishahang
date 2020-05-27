58772196570bdc6f0b65c4b11a15d108b3ae2c73
bjoluc <mail@bjoluc.de>
2020-05-27 18:58:17 +0200

    Remove confusing output from generate-env.sh

--- generate-env.sh
+++ generate-env.sh
@@ -5,9 +5,6 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-if [ -z "$2" ]; then
-    echo "PUBLIC_DOMAIN_NAME is empty, defaulting to \"$1\"."
-fi
 PUBLIC_DOMAIN_NAME=${2:-$1}
 
 sed -e "s/%{PUBLIC_IP}%/$1/g" -e "s/%{PUBLIC_DOMAIN_NAME}%/$PUBLIC_DOMAIN_NAME/" ./.env.template > .env
