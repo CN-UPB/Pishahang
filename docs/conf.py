0bff30280e2f16c130b89bd8bf8489a6d34bc753
bjoluc <mail@bjoluc.de>
2020-05-27 18:55:09 +0200

    Move figures directory into docs

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".venv"]
+exclude_patterns = ["_build", "package", "Thumbs.db", ".DS_Store", ".venv"]
 
 extlinks = {"sourcefile": ("https://github.com/CN-UPB/Pishahang/blob/master/%s", "")}
 
@@ -52,7 +52,7 @@ html_theme_options = {
     "titles_only": False,
 }
 
-html_logo = "../figures/pishahang-logo.svg"
+html_logo = "./figures/pishahang-logo.svg"
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
