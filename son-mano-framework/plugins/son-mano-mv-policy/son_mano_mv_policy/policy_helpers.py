[Fix] Integer normalization [Fix] Colorblind scheme

son-mano-framework/plugins/son-mano-mv-policy/son_mano_mv_policy/policy_helpers.py
@@ -56,8 +56,8 @@ def get_supported_versions(prediction, versions):
 Interpolate data points to a certain range
 '''
 def interpolate_array(values, min=_SCORE_MIN, max=_SCORE_MAX):
-    return np.interp(values, (values.min(), values.max()), (min, max))
-
+    _arr = np.interp(values, (values.min(), values.max()), (min, max))    
+    return np.ceil(_arr)
 
 '''
 Build the decision matrix for a given traffic prediction values 

son-mano-framework/plugins/son-mano-traffic-forecast/notebooks/Policy/policy_generator_WPM.py
@@ -138,7 +138,8 @@ Interpolate data points to a certain range
 
 
 def interpolate_array(values, min=_SCORE_MIN, max=_SCORE_MAX):
-    return np.interp(values, (values.min(), values.max()), (min, max))
+    _arr = np.interp(values, (values.min(), values.max()), (min, max))    
+    return np.ceil(_arr)
 
 
 '''
@@ -160,9 +161,9 @@ def build_decision_matrix(prediction, meta, versions):
 
             # Support deviation
             if _vm_version_value['max_data_rate'] > (prediction['mean'] + prediction['std']):
-                _decision_matrix[_vm_type_key][_vm_version_key]["support_deviation"] = 5
+                _decision_matrix[_vm_type_key][_vm_version_key]["support_deviation"] = _SCORE_MAX
             else:
-                _decision_matrix[_vm_type_key][_vm_version_key]["support_deviation"] = 1
+                _decision_matrix[_vm_type_key][_vm_version_key]["support_deviation"] = _SCORE_MIN
 
             # Over Provision
             _decision_matrix[_vm_type_key][_vm_version_key]["over_provision"] = int(
@@ -170,24 +171,24 @@ def build_decision_matrix(prediction, meta, versions):
 
             # Same Version
             if meta["current_version"] == _vm_version_key:
-                _decision_matrix[_vm_type_key][_vm_version_key]["same_version"] = 5
+                _decision_matrix[_vm_type_key][_vm_version_key]["same_version"] = _SCORE_MAX
             else:
-                _decision_matrix[_vm_type_key][_vm_version_key]["same_version"] = 1
+                _decision_matrix[_vm_type_key][_vm_version_key]["same_version"] = _SCORE_MIN
 
             # Overhead
             _decision_matrix[_vm_type_key][_vm_version_key]["overhead"] = _vm_version_value['management_overhead']
 
             # Support max datarate
             if _vm_version_value['max_data_rate'] >= (prediction['max']):
-                _decision_matrix[_vm_type_key][_vm_version_key]["support_max"] = 5
+                _decision_matrix[_vm_type_key][_vm_version_key]["support_max"] = _SCORE_MAX
             else:
-                _decision_matrix[_vm_type_key][_vm_version_key]["support_max"] = 1
+                _decision_matrix[_vm_type_key][_vm_version_key]["support_max"] = _SCORE_MIN
 
             # Support recent history
             if _vm_version_value['max_data_rate'] >= (meta["recent_history"]["mean"]):
-                _decision_matrix[_vm_type_key][_vm_version_key]["support_recent_history"] = 5
+                _decision_matrix[_vm_type_key][_vm_version_key]["support_recent_history"] = _SCORE_MAX
             else:
-                _decision_matrix[_vm_type_key][_vm_version_key]["support_recent_history"] = 1
+                _decision_matrix[_vm_type_key][_vm_version_key]["support_recent_history"] = _SCORE_MIN
 
     decision_matrix_df = pd.DataFrame.from_dict({(i, j): _decision_matrix[i][j]
                                                  for i in _decision_matrix.keys()
@@ -956,17 +957,17 @@ fig.savefig("./results/manual_variation_decision_plot.png",
 
 
 # %%
-
+_SCORE_MIN, _SCORE_MAX = 1, 9
 _acc_pc = "pc_100"
 _results = traffic_grouped[_acc_pc].copy()
 
 # iterate over the dataframe row by row and set version
-prediction = { "mean": 350, "std": 0, "min": 350, "max": 350 }
+prediction = { "mean": 350, "std": 150, "min": 350, "max": 650 }
 prediction_history = { "mean": 1000, "std": 0, "min": 1000, "max": 1000 }
 
 meta = {
-    "current_version": "transcoder-image-1-con",
-    "current_version_history_grouped": "transcoder-image-1-con",
+    "current_version": "transcoder-image-1-gpu",
+    "current_version_history_grouped": "transcoder-image-1-gpu",
     "recent_history": prediction_history
 }
 
@@ -980,15 +981,15 @@ decision_matrix_df = build_decision_matrix(
 # weights = WEIGHTS
 weights = {
     "negative": {
-        "cost": 9,
-        "over_provision": 9,
-        "overhead": 5
+        "cost": 5,
+        "over_provision": 5,
+        "overhead": 7
     },
     "positive": {
-        "support_deviation": 4,
-        "same_version": 5,
-        "support_max": 1,
-        "support_recent_history": 1
+        "support_deviation": 7,
+        "same_version": 7,
+        "support_max": 7,
+        "support_recent_history": 0
     }
 }
 
@@ -1019,8 +1020,9 @@ criteria = [c_cost, c_support_deviation, c_over_provision,
 
 list(decision_matrix_df.index)
 
-get_policy_decision_mcda
+# get_policy_decision_mcda
 
+# %%
 print("WSM")
 data = Data(decision_matrix_df.values, criteria,
             weights=weights,
@@ -1031,6 +1033,7 @@ dm = simple.WeightedSum()
 dec = dm.decide(data)
 print(data.anames[dec.best_alternative_])
 
+# %%
 print("WPM")
 data = Data(decision_matrix_df.values, criteria,
             weights=weights,
@@ -1041,6 +1044,7 @@ dm = simple.WeightedProduct()
 dec = dm.decide(data)
 print(data.anames[dec.best_alternative_])
 
+# %%
 print("TOPSIS")
 data = Data(decision_matrix_df.values, criteria,
             weights=weights,

son-mano-framework/plugins/son-mano-traffic-forecast/notebooks/Policy/policy_generator_runs.py
@@ -24,7 +24,7 @@ LIMIT_DATASET = False
 LOOK_AHEAD = 5  # Mins (factor of shape)
 EXPERIMENT_RUNS = 30
 
-FOLDER_REF = "report-price-fix-extended-30runs-2"
+FOLDER_REF = "report-topsis-extended-30runs-1"
 MCDA_METHOD = "TOPSIS"
 if not os.path.exists("./results/{}".format(FOLDER_REF)):
     os.makedirs("./results/{}/data".format(FOLDER_REF))
@@ -139,7 +139,8 @@ Interpolate data points to a certain range
 
 
 def interpolate_array(values, min=_SCORE_MIN, max=_SCORE_MAX):
-    return np.interp(values, (values.min(), values.max()), (min, max))
+    _arr = np.interp(values, (values.min(), values.max()), (min, max))    
+    return np.ceil(_arr)
 
 
 '''

son-mano-framework/plugins/son-mano-traffic-forecast/notebooks/Policy/policy_generator_scatter.py
@@ -38,9 +38,9 @@ WEIGHTS = {
         "overhead": 1
     },
     "positive": {
-        "support_deviation": 10,
-        "same_version": 5,
-        "support_max": 8,
+        "support_deviation": 1,
+        "same_version": 2,
+        "support_max": 1,
         "support_recent_history": 0
     }
 }
@@ -135,8 +135,8 @@ Interpolate data points to a certain range
 
 
 def interpolate_array(values, min=_SCORE_MIN, max=_SCORE_MAX):
-    return np.interp(values, (values.min(), values.max()), (min, max))
-
+    _arr = np.interp(values, (values.min(), values.max()), (min, max))    
+    return np.ceil(_arr)
 
 '''
 Build the decision matrix for a given traffic prediction values 
@@ -823,9 +823,20 @@ for _traffic_file in traffic_files:
 # https://stackoverflow.com/questions/53766397/how-to-center-the-grid-of-a-plot-on-scatter-points
 # https://stackoverflow.com/questions/47684652/how-to-customize-marker-colors-and-shapes-in-scatter-plot
 markers = ["v" , "^" , "v" , ">" , "^" , "<", ">"]
-colors = ['r','g','b','c','m', 'y', 'k']
+colors = ['#0173b2',
+ '#de8f05',
+ '#029e73',
+ '#d55e00',
+ '#cc78bc',
+ '#ca9161',
+ '#fbafe4',
+ '#949494',
+ '#ece133',
+ '#56b4e9']
 
 sns.reset_orig()
+sns.set_palette("colorblind")
+
 fig, ax = plt.subplots(5, figsize=(8,8))
 _counter = 0
 for _traffic_file in traffic_files:
@@ -899,7 +910,7 @@ fig.savefig("./results/manual_variation_decision_scatter.png",
             dpi=300)
 
 # %%
-sns.set_style("darkgrid")
+sns.set_style("whitegrid")
 fig, ax = plt.subplots(5, figsize=(8,8))
 _counter = 0
 for _traffic_file in traffic_files:
@@ -926,8 +937,8 @@ for _traffic_file in traffic_files:
     ax[_counter].get_xaxis().set_minor_locator(plticker.AutoMinorLocator())
     ax[_counter].get_yaxis().set_minor_locator(plticker.AutoMinorLocator())
     ax[_counter].grid(b=False)
-    ax[_counter].grid(b=True, axis="x", which='minor', color='w', linewidth=1.0)
-    ax[_counter].grid(b=True, axis="x", which='major', color='w', linewidth=1.0)
+    ax[_counter].grid(b=True, axis="x", which='minor', color='lightgrey', linewidth=1.0)
+    ax[_counter].grid(b=True, axis="x", which='major', color='lightgrey', linewidth=1.0)
 
     _counter += 1
 