83ef7a533d49f4ecfd824af65ea170acc0f38a96
bjoluc <mail@bjoluc.de>
2020-06-08 12:24:17 +0200

    Remove unused versiondescriptor page

--- src/gui/pages/versiondescriptor.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from "next";
-
-import { Page } from "../lib/components/layout/Page";
-
-const versiondescriptor: NextPage = () => {
-  return <Page title="Version Descriptors">Coming Soon...</Page>;
-};
-
-export default versiondescriptor;
