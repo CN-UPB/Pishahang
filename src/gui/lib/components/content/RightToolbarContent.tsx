ae12c8f4a78be92c617545caa93814069d628716
bjoluc <mail@bjoluc.de>
2020-06-08 10:59:07 +0200

    Add the user's full name to the toolbar

--- src/gui/lib/components/content/RightToolbarContent.tsx
+++ src/gui/lib/components/content/RightToolbarContent.tsx
@@ -1,18 +1,24 @@
-import { IconButton, Tooltip } from "@material-ui/core";
+import { IconButton, Tooltip, Typography, useTheme } from "@material-ui/core";
 import { PowerSettingsNew as LogoutIcon, Person as PersonIcon } from "@material-ui/icons";
 import * as React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { logout } from "../../store/actions/auth";
+import { selectUserFullName } from "../../store/selectors/auth";
 import { IconButtonLink } from "../links/IconButtonLink";
 
 export const RightToolbarContent: React.FunctionComponent<React.HTMLAttributes<HTMLDivElement>> = (
   props
 ) => {
+  const theme = useTheme();
   const dispatch = useDispatch();
+  const userName = useSelector(selectUserFullName);
 
   return (
     <div {...props}>
+      <Typography noWrap style={{ alignSelf: "center", marginRight: theme.spacing(1) }}>
+        {userName}
+      </Typography>
       <Tooltip title="Profile" arrow>
         <IconButtonLink color={"inherit"} href="/user">
           <PersonIcon />
