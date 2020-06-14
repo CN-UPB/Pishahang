5ddc52dea39042b2ca6042c9006b6f2174a0db25
bjoluc <mail@bjoluc.de>
2020-06-14 10:17:09 +0200

    Remove old vim-adaptor

--- vim-adaptor/adaptor/src/main/resources/templates/kubernetes/main.tf
+++
@@ -1,8 +0,0 @@
-provider "kubernetes" {
-  load_config_file = "false"
-
-  host = "{{ endpoint }}"
-
-  token = "{{ token }}"
-  insecure = true
-}
--- vim-adaptor/son-sp-infra-stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pid=`ps aux | grep adaptor | awk '{print $2}'`
-kill -9 $pid
