f567482fa9899e2fb4727ea094dbfdc109145809
bjoluc <mail@bjoluc.de>
2020-06-15 17:54:47 +0200

    Migrate VimsTable to SwrDataTable

--- src/gui/lib/components/content/tables/VimsTable.tsx
+++ src/gui/lib/components/content/tables/VimsTable.tsx
@@ -1,30 +1,22 @@
-import {
-  Fab,
-  IconButton,
-  Paper,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Tooltip,
-} from "@material-ui/core";
-import { Add, DeleteForeverRounded, InfoRounded } from "@material-ui/icons";
+import { useTheme } from "@material-ui/core";
+import { Add, DeleteForeverRounded, Info } from "@material-ui/icons";
 import React from "react";
 
 import { ApiDataEndpoint } from "../../../api/endpoints";
-import { InjectedAuthorizedSWRProps, withAuthorizedSWR } from "../../../hocs/withAuthorizedSWR";
 import { useGenericConfirmationDialog } from "../../../hooks/genericConfirmationDialog";
 import { useAddVimDialog } from "../../../hooks/useAddVimDialog";
+import { useAuthorizedSWR } from "../../../hooks/useAuthorizedSWR";
+import { RetrievedVim } from "../../../models/Vim";
 import { useThunkDispatch } from "../../../store";
 import { showVimInfoDialog } from "../../../store/actions/dialogs";
 import { deleteVim } from "../../../store/thunks/vims";
-import { Table } from "../../layout/tables/Table";
+import { SwrDataTable } from "../../layout/tables/SwrDataTable";
 
-type Props = InjectedAuthorizedSWRProps<ApiDataEndpoint.Vims>;
-
-const InternalVimsTable: React.FunctionComponent<Props> = ({ data: vims, revalidate }) => {
+export const VimsTable: React.FunctionComponent = () => {
   const dispatch = useThunkDispatch();
+  const swr = useAuthorizedSWR(ApiDataEndpoint.Vims);
+  const showAddVimDialog = useAddVimDialog(swr.revalidate);
+  const theme = useTheme();
 
   const showRemoveVimDialog = useGenericConfirmationDialog(
     "Remove VIM?",
@@ -34,69 +26,42 @@ const InternalVimsTable: React.FunctionComponent<Props> = ({ data: vims, revalid
 
       let reply = await dispatch(deleteVim(id, { successSnackbarMessage: "VIM removed" }));
       if (reply.success) {
-        revalidate();
+        swr.revalidate();
       }
     },
     "Remove VIM"
   );
 
-  const showAddVimDialog = useAddVimDialog(revalidate);
-
   return (
-    <>
-      <Tooltip title="Add a VIM" arrow>
-        <Fab
-          color="primary"
-          size="small"
-          style={{ float: "right" }}
-          aria-label="Add a virtual infrastructure manager"
-          onClick={showAddVimDialog}
-        >
-          <Add />
-        </Fab>
-      </Tooltip>
-      <TableContainer component={Paper}>
-        <Table aria-label="Virtual Infrastructure Managers">
-          <TableHead>
-            <TableRow>
-              <TableCell align="left">Name</TableCell>
-              <TableCell align="center">Country</TableCell>
-              <TableCell align="center">City</TableCell>
-              <TableCell align="center">Core Usage</TableCell>
-              <TableCell align="center">Memory Usage</TableCell>
-              <TableCell align="center" style={{ width: "200px" }}>
-                Actions
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {vims.map((vim) => (
-              <TableRow key={vim.id}>
-                <TableCell align="left">{vim.name}</TableCell>
-                <TableCell align="center">{vim.country}</TableCell>
-                <TableCell align="center">{vim.city}</TableCell>
-                <TableCell align="center">{vim.coresUsed + "/" + vim.coresTotal}</TableCell>
-                <TableCell align="center">{vim.memoryUsed + "/" + vim.memoryTotal}</TableCell>
-                <TableCell align="center">
-                  <Tooltip title="Info" arrow>
-                    <IconButton color="primary" onClick={() => dispatch(showVimInfoDialog(vim))}>
-                      <InfoRounded />
-                    </IconButton>
-                  </Tooltip>
-
-                  <Tooltip title={"Remove " + vim.name} arrow>
-                    <IconButton color="secondary" onClick={() => showRemoveVimDialog(vim.id)}>
-                      <DeleteForeverRounded />
-                    </IconButton>
-                  </Tooltip>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
+    <SwrDataTable
+      swr={swr}
+      columns={[
+        { title: "Name", field: "name" },
+        { title: "Country", field: "country" },
+        { title: "City", field: "city" },
+        { title: "Core Usage", render: (vim) => vim.coresUsed + "/" + vim.coresTotal },
+        { title: "Memory Usage", render: (vim) => vim.memoryUsed + "/" + vim.memoryTotal + " MB" },
+      ]}
+      actions={[
+        {
+          icon: (props) => <Info htmlColor={theme.palette.primary.main} {...props} />,
+          tooltip: "Info",
+          onClick: (event, vim: RetrievedVim) => dispatch(showVimInfoDialog(vim)),
+        },
+        (vim) => ({
+          icon: (props) => (
+            <DeleteForeverRounded htmlColor={theme.palette.secondary.main} {...props} />
+          ),
+          tooltip: "Delete " + vim.name,
+          onClick: () => showRemoveVimDialog(vim.id),
+        }),
+        {
+          icon: (props) => <Add {...props} />,
+          tooltip: "Add a VIM",
+          onClick: showAddVimDialog,
+          isFreeAction: true,
+        },
+      ]}
+    />
   );
 };
-
-export const VimsTable = withAuthorizedSWR(ApiDataEndpoint.Vims)(InternalVimsTable);
