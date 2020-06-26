da6aa3ca3a81f76bd542f7b65560c172ad3b27b8
bjoluc <mail@bjoluc.de>
2020-06-26 19:47:54 +0200

    Upgrade terraform to version 0.12.28

--- src/mano-framework/plugins/vim-adaptor/install_terraform.sh
+++ src/mano-framework/plugins/vim-adaptor/install_terraform.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-curl https://releases.hashicorp.com/terraform/0.12.25/terraform_0.12.25_linux_amd64.zip \
+VERSION=0.12.28
+
+curl "https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip" \
     -o terraform.zip -sS
 unzip -qo terraform.zip
 rm terraform.zip
