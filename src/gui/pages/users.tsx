d6f19b3478c5f8cced5e61f59f21250ff23c76d3
basit <basit@entangle09.cs.uni-paderborn.de>
2020-06-29 14:13:47 +0200

    Created a users Page to display list of users, WIP.

---
+++ src/gui/pages/users.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+import { Page } from "../lib/components/layout/Page";
+
+const DashboardPage: NextPage = () => {
+  return <Page title="Users"></Page>;
+};
+
+export default DashboardPage;
