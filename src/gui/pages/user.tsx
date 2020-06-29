57ffd4dc6cab77297c5565fce472c9b319312990
basit <basit@entangle09.cs.uni-paderborn.de>
2020-06-29 14:14:19 +0200

    renamed user page to profile

--- src/gui/pages/user.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from "next";
-
-import { Page } from "../lib/components/layout/Page";
-
-const DashboardPage: NextPage = () => {
-  return <Page title="Your Profile"></Page>;
-};
-
-export default DashboardPage;
