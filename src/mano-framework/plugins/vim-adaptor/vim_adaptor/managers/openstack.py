cd14279f37e4e8422251ac26e3c3cf979960b2ed
bjoluc <mail@bjoluc.de>
2020-07-06 16:21:15 +0200

    Fix OpenStack terraform vars

--- src/mano-framework/plugins/vim-adaptor/vim_adaptor/managers/openstack.py
+++ src/mano-framework/plugins/vim-adaptor/vim_adaptor/managers/openstack.py
@@ -18,8 +18,8 @@ class OpenStackFunctionInstanceManager(TerraformFunctionInstanceManager):
         return {
             "auth_url": "http://{}/identity".format(vim.address),
             "tenant_id": vim.tenant.id,
-            "user_name": vim.username,
-            "user_password": vim.password,
+            "username": vim.username,
+            "password": vim.password,
             "network_id": vim.tenant.external_network_id,
             "router_id": vim.tenant.external_network_id,
         }
--- src/mano-framework/plugins/vim-adaptor/vim_adaptor/templates/openstack/var.tf
+++ src/mano-framework/plugins/vim-adaptor/vim_adaptor/templates/openstack/var.tf
@@ -1,20 +1,15 @@
-variable user_name {
+variable username {
     description = "The Username to login with."
 }
 
-variable tenant_name {
+variable tenant_id {
     description = "The ID of the Tenant (Identity v2) or Project (Identity v3) to login with."
 }
 
 variable password {
-    description = "(Optional) The Password to login with."
+    description = "The Password to login with."
 }
 
 variable auth_url {
     description = "The Identity authentication URL."
-}
-
-variable region {
-    description = "The region of the OpenStack cloud to use."
-    default = "RegionOne"
 }
