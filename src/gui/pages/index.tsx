a967c057aefac5c6fdf9bd9e9ba5ec80c7743a69
bjoluc <mail@bjoluc.de>
2020-07-29 12:44:37 +0200

    Remove placeholder graph from dashboard

--- src/gui/pages/index.tsx
+++ src/gui/pages/index.tsx
@@ -1,14 +1,9 @@
 import { NextPage } from "next";
 
-import DashBoardGraph from "../lib/components/layout/charts/DashboardGraph";
 import { Page } from "../lib/components/layout/Page";
 
 const DashboardPage: NextPage = () => {
-  return (
-    <Page title="Dashboard">
-      <DashBoardGraph></DashBoardGraph>
-    </Page>
-  );
+  return <Page title="Dashboard"></Page>;
 };
 
 export default DashboardPage;
