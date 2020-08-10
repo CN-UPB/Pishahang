d8b48cdd0b8e5a1d78f47c1d9b9091535d2bc2b5
bjoluc <mail@bjoluc.de>
2020-08-10 16:04:43 +0200

    Add `update` function to manobase.repository

--- src/mano-framework/base/manobase/repository.py
+++ src/mano-framework/base/manobase/repository.py
@@ -2,7 +2,7 @@
 Helper functions to access the repository microservice
 """
 
-from typing import List, Union
+from typing import Callable, List, Union
 
 import requests
 from appcfg import get_config
@@ -12,8 +12,10 @@ repository_url = get_config(__name__)["repository_url"]
 
 TIMEOUT = 5  # seconds
 
+Resource = Union[dict, List[dict]]
 
-def post(endpoint: str, data: Union[dict, List[dict]]):
+
+def post(endpoint: str, data: Resource):
     """
     Sends a POST request to the given repository endpoint with the provided data and
     raises a `requests.RequestException` on error.
@@ -27,7 +29,7 @@ def post(endpoint: str, data: Union[dict, List[dict]]):
     return response.json()
 
 
-def put(endpoint: str, data: Union[dict, List[dict]]):
+def put(endpoint: str, data: Resource):
     """
     Sends a PUT request to the given repository endpoint with the provided data and
     raises a `requests.RequestException` on error.
@@ -41,7 +43,7 @@ def put(endpoint: str, data: Union[dict, List[dict]]):
     return response.json()
 
 
-def get(endpoint: str):
+def get(endpoint: str) -> Resource:
     """
     Returns the deserialized JSON data retrieved from the provided repository endpoint
     or raises a `requests.RequestException` on error.
@@ -66,3 +68,20 @@ def delete(endpoint: str):
     response = requests.delete(f"{repository_url}/{endpoint}", timeout=TIMEOUT)
     response.raise_for_status()
     return response.json()
+
+
+def update(endpoint: str, updater: Callable[[Resource], Resource]) -> Resource:
+    """
+    Retrieves the resource from `endpoint`, calls the provided `updater` function and
+    sends a PUT request with the return value from the `updater` function to the
+    endpoint. On error, a `requests.RequestException` is raised.
+
+    Args:
+        endpoint: The endpoint URL relative to the repository root URL, without a leading slash
+        updater: A callback function that will be invoked with the data retrieved from the endpoint and returns the updated data that will be sent in a PUT request
+
+    Returns: The updated resource as returned by the `updater` function
+    """
+    resource = updater(get(endpoint))
+    put(endpoint, resource)
+    return resource
